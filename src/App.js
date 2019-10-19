0a99704094c0e709187d3ad0ea3a44f7e3a7619c
Rohit Bandooni <rbandoo@C02YT5WMLVCF.local>
2019-10-19 02:25:37 -0400

    Header and body first version

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import Body from './components/body';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
   return (
-    <Header />
+    <Grid>
+      <Header />
+      <Body />
+    </Grid>
+
   );
 }
 
