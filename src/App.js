540b335ecf868d76f71a233e4e8899abc4f5168a
Rohit Bandooni <Rohit_Bandooni@ncsu.edu>
2019-11-08 23:11:09 -0500

    Basic Layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React from "react";
 // import logo from './logo.svg';
-import './App.css';
-import Header from './components/header';
-import Body from './components/body';
-import Grid from '@material-ui/core/Grid';
+import "./App.css";
+import Header from "./components/header";
+import Body from "./components/body";
+import Grid from "@material-ui/core/Grid";
+import Navigation from "./components/navigation";
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
     <Grid>
-      <Header />
-      <hr />
-      <div style={{ paddingTop: '20px' }}></div>
-      <Body />
-    </Grid>
+      {/* Our header.js file exports the reusable Header component. 
+      It is just a simple Grid based header with logo on the left hand side and typography on the right  */}
 
+      <Header />
+
+      {/* 
+      Our Main part of this application would be this Navigation component. It contains code for our main navigation
+      It is using a material-ui tab panel for simplicity and ease of use. 
+      Each tab contains/would-contain components individually, as required.
+      */}
+
+      <Navigation />
+
+      {/* body.js is just a temporary code, that would eventually be replaced by footer.js 
+      Until production, this can serve as a playground!!
+      */}
+
+      {/* <Body /> */}
+
+      <Footer />
+    </Grid>
   );
 }
 
