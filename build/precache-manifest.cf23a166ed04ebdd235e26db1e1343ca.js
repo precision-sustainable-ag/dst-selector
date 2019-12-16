d0a9f067e25c2e848ccee7468762f443bc4811de
Rohit Bandooni <rbandoo@ncsu.edu>
2019-12-16 16:52:47 -0500

    build milestone 1.1

---
+++ build/precache-manifest.cf23a166ed04ebdd235e26db1e1343ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661232924e6407f0e030d27d53f59082",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56079ba0c923af725a89",
+    "url": "/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "f3048b621f9d93a7be6e",
+    "url": "/static/css/main.57ea4eea.chunk.css"
+  },
+  {
+    "revision": "56079ba0c923af725a89",
+    "url": "/static/js/2.215b18f1.chunk.js"
+  },
+  {
+    "revision": "f3048b621f9d93a7be6e",
+    "url": "/static/js/main.b6f96695.chunk.js"
+  },
+  {
+    "revision": "c7b1804daca714060507",
+    "url": "/static/js/runtime-main.0c153682.js"
+  }
+]);
