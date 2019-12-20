6e5a5873a176973317f5b5348a45a3af4481c43c
Rohit Bandooni <rbandoo@ncsu.edu>
2019-12-20 12:53:53 -0500

    header responsiveness fix

--- src/components/GreenBar/greenBarComponent.js
+++ src/components/GreenBar/greenBarComponent.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
-import { Typography, GridList, GridListTile } from "@material-ui/core";
-// import LocationOnIcon
+import { Typography, GridList, GridListTile, Button } from "@material-ui/core";
+import "../../styles/greenBar.css";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+
 export class GreenBarComponent extends Component {
   constructor(props) {
     super(props);
@@ -28,28 +30,62 @@ export class GreenBarComponent extends Component {
       this.flag = true;
     }
   };
+  locationIcon = (w,h) => {
+    return(<svg width={w} height={h} viewBox="0 0 14 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7 0C3.13 0 0 3.13 0 7C0 12.25 7 20 7 20C7 20 14 12.25 14 7C14 3.13 10.87 0 7 0ZM7 9.5C5.62 9.5 4.5 8.38 4.5 7C4.5 5.62 5.62 4.5 7 4.5C8.38 4.5 9.5 5.62 9.5 7C9.5 8.38 8.38 9.5 7 9.5Z" fill="white"/>
+    </svg>
+    );
+  }
+  zoneIcon = (w, h) => {
+    return (
+      <svg
+        height={h}
+        width={w}
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM6 15.5C4.62 15.5 3.5 14.38 3.5 13C3.5 11.62 4.62 10.5 6 10.5C7.38 10.5 8.5 11.62 8.5 13C8.5 14.38 7.38 15.5 6 15.5ZM7.5 6C7.5 4.62 8.62 3.5 10 3.5C11.38 3.5 12.5 4.62 12.5 6C12.5 7.38 11.38 8.5 10 8.5C8.62 8.5 7.5 7.38 7.5 6ZM14 15.5C12.62 15.5 11.5 14.38 11.5 13C11.5 11.62 12.62 10.5 14 10.5C15.38 10.5 16.5 11.62 16.5 13C16.5 14.38 15.38 15.5 14 15.5Z"
+          fill="white"
+        />
+      </svg>
+    );
+  };
+
+  formattedAddress = () => {
+    let address = this.props.wellState.address.toString().substring(0, 19);
+    return (
+      <div className="addressBar">
+        <Button size="small">
+        {this.locationIcon(20,20)}
+        &nbsp; {address}
+        </Button>
+      </div>
+    );
+  };
+  formatterZone = () => {
+    return (
+      // <div className="zoneBar">Plant Hardiness {this.props.wellState.zoneText}</div>
+      <div className="zoneBar">
+        <Button size="small">
+          {this.zoneIcon(20, 20)}
+          &nbsp; Plant Hardiness {this.props.wellState.zoneText}
+        </Button>
+      </div>
+    );
+  };
   render() {
     return (
-      <GridList
-        container
-        style={{ backgroundColor: "#598444", height: "40px" }}
-        children={2}
-      >
-        <GridListTile item>
-          <p>
-            {this.flag === true
-              ? this.props.wellState.address.toString().substring(0, 19)
-              : ""}
-          </p>
-        </GridListTile>
-        <GridListTile>
-          <p>
-            {this.flag === true
-              ? `Plant Hardiness ${this.props.wellState.zoneText}`
-              : ""}
-          </p>
-        </GridListTile>
-      </GridList>
+      <div className="greenBarContainer">
+        {this.flag === true && this.props.wellState.address !== "Enter Address"
+          ? this.formattedAddress()
+          : ""}
+
+        {this.flag === true && this.props.wellState.zoneText !== undefined
+          ? this.formatterZone()
+          : ""}
+      </div>
     );
   }
 }
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -7,9 +7,13 @@ import {
   Link,
   List,
   ListItem,
-  ListItemText
+  ListItemText,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Button
 } from "@material-ui/core";
-
+import { MenuIcon } from "@material-ui/icons/Menu";
 import { makeStyles } from "@material-ui/core/styles";
 import { DateComponent } from "./dateComponent";
 import { GreenBarComponent } from "../GreenBar/greenBarComponent";
@@ -27,8 +31,11 @@ class Header extends Component {
   }
 
   progressInterval;
+  // BUG(Rohit Bandooni): Responsiveness, breakpoints have not been set
+  // BUG(Rohit Bandooni): logo cropping on smaller screens
+  // TODO: Alternative for navigation? with hamberger menu ?
 
-  componentDidMount() {
+  setProgressBackground = () => {
     this.progressInterval = setInterval(() => {
       var a = localStorage.getItem("stepperState");
       a = JSON.parse(a);
@@ -54,6 +61,11 @@ class Header extends Component {
       });
       // console.log(this.progressInterval);
     }, 100);
+    return this.progressInterval;
+  };
+
+  componentDidMount() {
+    this.setProgressBackground();
   }
   componentWillUnmount() {
     clearTimeout(this.progressInterval);
@@ -66,99 +78,161 @@ class Header extends Component {
       backgroundImage: `url(${this.props.logoPath})`
     };
     return (
-      <header>
-        <Container maxWidth="xl">
-          {/* This banner is good for desktop sizes, need something different for mobile, or probably ged rid of the borders */}
-          <div id="banner">
-            <ul>
-              <li>
-                <a href="">ABOUT</a>
-              </li>
-              <li>
-                <a href="">NECCC</a>
-              </li>
-              <li>
-                <a href="">USDA NRCS</a>
-              </li>
-              <li>
-                <a href="">NE SARE</a>
-              </li>
-              <li>
-                <a href="">HELP</a>
-              </li>
-              <li>
-                <a href="">FEEDBACK</a>
-              </li>
-            </ul>
-          </div>
+      // <header>
+      //   <Grid container maxWidth="xl">
+      //     {/* This banner is good for desktop sizes, need something different for mobile, or probably ged rid of the borders */}
+      //     <div id="banner">
+      // <ul>
+      //   <li>
+      //     <a href="">ABOUT</a>
+      //   </li>
+      //   <li>
+      //     <a href="">NECCC</a>
+      //   </li>
+      //   <li>
+      //     <a href="">USDA NRCS</a>
+      //   </li>
+      //   <li>
+      //     <a href="">NE SARE</a>
+      //   </li>
+      //   <li>
+      //     <a href="">HELP</a>
+      //   </li>
+      //   <li>
+      //     <a href="">FEEDBACK</a>
+      //   </li>
+      // </ul>
+      //     </div>
 
-          <Grid container direction="row" alignItems="center">
-            <Grid item md={2} className="logoGrid">
-              <Grid item md={12} style={logo}></Grid>
-            </Grid>
-            <Grid
-              item
-              md={3}
-              className="dateGrid"
-              style={{ paddingLeft: "1em" }}
-            >
+      //     <Grid container direction="row" alignItems="center">
+      //       <Grid item md={3} sm={12} className="logoGrid">
+      //         <Grid item md={12} style={logo}></Grid>
+      //       </Grid>
+      //       <Grid
+      //         item
+      //         md={3}
+      //         className="dateGrid"
+      //         style={{ paddingLeft: "1em" }}
+      //       >
+      //         <DateComponent />
+      //       </Grid>
+      //     </Grid>
+      //     <div style={{ padding: "15px" }}></div>
+      //     <List component="nav" style={flexContainer}>
+      //       <ListItem button className="listitemButton itemButton1">
+      //         <ListItemText
+      //           disableTypography
+      //           primary={
+      //             <Typography style={{ fontWeight: "bold", fontSize: "15pt" }}>
+      //               COVER CROP EXPLORER
+      //             </Typography>
+      //           }
+      //         />
+      //       </ListItem>
+      //       <ListItem button className="listitemButton itemButton2">
+      //         <ListItemText
+      //           disableTypography
+      //           primary={
+      //             <Typography style={{ fontWeight: "bold", fontSize: "15pt" }}>
+      //               SPECIES SELECTOR
+      //             </Typography>
+      //           }
+      //         />
+      //       </ListItem>
+      //       <ListItem button className="listitemButton itemButton3">
+      //         <ListItemText
+      //           disableTypography
+      //           primary={
+      //             <Typography style={{ fontWeight: "bold", fontSize: "15pt" }}>
+      //               MIX MAKER
+      //             </Typography>
+      //           }
+      //         />
+      //       </ListItem>
+      //       <ListItem button className="listitemButton itemButton4">
+      //         <ListItemText
+      //           disableTypography
+      //           primary={
+      //             <Typography style={{ fontWeight: "bold", fontSize: "15pt" }}>
+      //               SEED RATE CALCULATOR
+      //             </Typography>
+      //           }
+      //         />
+      //       </ListItem>
+      //       <ListItem button className="listitemButton itemButton5">
+      //         <ListItemText
+      //           disableTypography
+      //           primary={
+      //             <Typography style={{ fontWeight: "bold", fontSize: "15pt" }}>
+      //               MY COVER CROP LIST
+      //             </Typography>
+      //           }
+      //         />
+      //       </ListItem>
+      //     </List>
+      //   </Grid>
+      //   <GreenBarComponent {...this.state} />
+      // </header>
+      <header>
+        <div className="topHeader">
+          <ul>
+            <li>
+              <a href="">ABOUT</a>
+            </li>
+            <li>
+              <a href="">NECCC</a>
+            </li>
+            <li>
+              <a href="">USDA NRCS</a>
+            </li>
+            <li>
+              <a href="">NE SARE</a>
+            </li>
+            <li>
+              <a href="">HELP</a>
+            </li>
+            <li>
+              <a href="">FEEDBACK</a>
+            </li>
+          </ul>
+        </div>
+        <div className="bottomHeader">
+          <section>
+            <div className="logoContainer">
+              <img src={this.props.logoPath}></img>
+            </div>
+            <div>
               <DateComponent />
-            </Grid>
-          </Grid>
-          <div style={{ padding: "15px" }}></div>
-          <List component="nav" style={flexContainer}>
-            <ListItem button className="listitemButton itemButton1">
-              <ListItemText
-                disableTypography
-                primary={
-                  <Typography style={{ fontWeight: "bold", fontSize: "15pt" }}>
-                    COVER CROP EXPLORER
-                  </Typography>
-                }
-              />
-            </ListItem>
-            <ListItem button className="listitemButton itemButton2">
-              <ListItemText
-                disableTypography
-                primary={
-                  <Typography style={{ fontWeight: "bold", fontSize: "15pt" }}>
-                    SPECIES SELECTOR
-                  </Typography>
-                }
-              />
-            </ListItem>
-            <ListItem button className="listitemButton itemButton3">
-              <ListItemText
-                disableTypography
-                primary={
-                  <Typography style={{ fontWeight: "bold", fontSize: "15pt" }}>
-                    MIX MAKER
-                  </Typography>
-                }
-              />
-            </ListItem>
-            <ListItem button className="listitemButton itemButton4">
-              <ListItemText
-                disableTypography
-                primary={
-                  <Typography style={{ fontWeight: "bold", fontSize: "15pt" }}>
-                    SEED RATE CALCULATOR
-                  </Typography>
-                }
-              />
-            </ListItem>
-            <ListItem button className="listitemButton itemButton5">
-              <ListItemText
-                disableTypography
-                primary={
-                  <Typography style={{ fontWeight: "bold", fontSize: "15pt" }}>
-                    MY COVER CROP LIST
-                  </Typography>
-                }
-              />
-            </ListItem>
-          </List>
-        </Container>
+            </div>
+          </section>
+          <ul>
+            {/* <li> */}
+            <Button size="large" className="listitemButton itemButton1">
+              COVER CROP EXPLORER
+            </Button>
+            {/* </li> */}
+            {/* <li className="active"> */}
+            <Button size="large" className="listitemButton itemButton2">
+              SPECIES SELECTOR
+            </Button>
+            {/* </li> */}
+            {/* <li> */}
+            <Button size="large" className="listitemButton itemButton3">
+              MIX MAKER
+            </Button>
+            {/* </li> */}
+            {/* <li> */}
+            <Button size="large" className="listitemButton itemButton4">
+              SEED RATE CALCULATOR
+            </Button>
+            {/* </li> */}
+            {/* <li> */}
+            <Button size="large" className="listitemButton itemButton5">
+              MY COVER CROP LIST
+            </Button>
+            {/* </li> */}
+          </ul>
+        </div>
         <GreenBarComponent {...this.state} />
       </header>
     );
