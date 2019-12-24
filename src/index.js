ae4a69b1a092d783cef9603daf1fae553b8ee7fe
Rohit Bandooni <rbandoo@ncsu.edu>
2019-12-23 20:44:20 -0500

    Fixes #8 and Fixes #9 and Fixes #22

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "leaflet-control-geocoder/dist/Control.Geocoder.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
