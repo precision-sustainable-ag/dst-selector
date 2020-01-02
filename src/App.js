15aba9aac226bd78d7f5346421f244cb6799d864
Rohit Bandooni <rbandoo@ncsu.edu>
2020-01-02 14:30:45 -0500

    Fixes #12 and some UI modifications

--- src/App.js
+++ src/App.js
@@ -16,32 +16,33 @@ const logoPath = "/images/neccc_wide_logo_color_web.jpg";
 function App() {
   // const isRootRoute = this.props.path == "/" ? true : false;
   return (
-    <Box component="div">
-      {/* Our header.js file exports the reusable Header component.
+    <div>
+      <Box component="div" className="mainContainer">
+        {/* Our header.js file exports the reusable Header component.
       It is just a simple Grid based header with logo on the left hand side and typography on the right  */}
 
-      <Header logoPath={`${logoPath}`} />
+        <Header logoPath={`${logoPath}`} />
 
-      <WellComponent />
-      {/* <BodyComponent /> */}
-      {/* {isRootRoute ? <Header logoPath={`${logoPath}`} /> : ""} */}
+        <WellComponent />
+        {/* <BodyComponent /> */}
+        {/* {isRootRoute ? <Header logoPath={`${logoPath}`} /> : ""} */}
 
-      {/*
+        {/*
       Our Main part of this application would be this Navigation component. It contains code for our main navigation
       It is using a material-ui tab panel for simplicity and ease of use.
       Each tab contains/would-contain components individually, as required.
       */}
 
-      {/* <Navigation /> */}
+        {/* <Navigation /> */}
 
-      {/* body.js is just a temporary code, that would eventually be replaced by footer.js
+        {/* body.js is just a temporary code, that would eventually be replaced by footer.js
       Until production, this can serve as a playground!!
       */}
 
-      {/* <Body /> */}
-
+        {/* <Body /> */}
+      </Box>
       <Footer />
-    </Box>
+    </div>
   );
 }
 
--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,95 +1,33 @@
 import React from "react";
-import {
-  Grid,
-  Container,
-  // Button,
-  // List,
-  // ListItem,
-  // ListItemText,
-  // Typography,
-  Link,
-  Box
-} from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "@material-ui/core";
 import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
-// import { width } from "@material-ui/system";
-// import Helper from "./helper";
-
-const useStyles = makeStyles(theme => ({
-  footer: {
-    position: `absolute`,
-    bottom: 0,
-    backgroundColor: `#598445`,
-    width: `100%`,
-    height: `5%`,
-    maxHeight: "5%"
-  },
-  textButton: {},
-
-  surface: {
-    backgroundColor: "rgba(0, 0, 0, 0)"
-  }
-}));
+// import "../../styles/header.css";
+import "../../styles/footer.css";
 
 function Footer() {
-  const classes = useStyles();
-  // const year = new Date().getFullYear();
   return (
-    // <footer className={classes.footer}>
-    //   <Container maxWidth="xl">
-    //     <Grid container alignItems="center">
-    //       <Box flexGrow={1}>
-    // <p>
-    // Disclaimer: Actual cover crop performance may vary. Consult an{" "}
-    // <a href="http://placehold.it/1000x1000" style={{ color: "#fff" }}>
-    //   NRCS Extension Educator
-    // </a>{" "}
-    // for detailed guidance.
-    // </p>
-    //       </Box>
-
-    //       <Box>
-    // <Link
-    //   href="https://open-source.org/"
-    //   style={{ paddingRight: "50px", color: "#000" }}
-    // >
-    //   OPEN SOURCE
-    // </Link>
-    // <Link
-    //   href="https://google.com/"
-    //   style={{ paddingRight: "50px", color: "#000" }}
-    // >
-    //   CONTACT US
-    // </Link>
-    // <Link
-    //   href="https://google.com"
-    //   style={{ paddingRight: "50px", color: "#000" }}
-    // >
-    //   2019
-    // </Link>
-    //       </Box>
-    //     </Grid>
-    //   </Container>
-    // </footer>
-    <MDBFooter
-      className="font-small pt-4 fixed-bottom"
-      style={{ backgroundColor: `#598445` }}
-    >
+    <MDBFooter className="primaryFooter font-small pt-4">
       <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
           <MDBCol md="6">
             {/* <h5 className="title">Footer Content</h5> */}
             <p>
               Disclaimer: Actual cover crop performance may vary. Consult an{" "}
-              <a href="http://placehold.it/1000x1000" style={{ color: "#fff" }}>
+              <a
+                className="footerLink"
+                href="http://placehold.it/1000x1000"
+                style={{
+                  color: "#fff"
+                }}
+              >
                 NRCS Extension Educator
               </a>{" "}
               for detailed guidance.
             </p>
           </MDBCol>
-          <MDBCol md="4" className="offset-md-2">
+          <MDBCol md="4" className="offset-md-2 rightSideFooterLinks">
             <Link
-              href="https://open-source.org/"
+              href="https://opensource.org/"
               style={{ paddingRight: "50px", color: "#000" }}
             >
               OPEN SOURCE
@@ -109,12 +47,6 @@ function Footer() {
           </MDBCol>
         </MDBRow>
       </MDBContainer>
-      {/* <div className="footer-copyright text-center py-3">
-        <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright:{" "}
-          <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
-        </MDBContainer>
-      </div> */}
     </MDBFooter>
   );
 }
