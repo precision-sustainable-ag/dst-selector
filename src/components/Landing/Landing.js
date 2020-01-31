64e22e02229b1496d65c73b523427e37927aea07
Rohit Bandooni <rbandoo@ncsu.edu>
2020-01-31 14:39:51 -0500

    Modified text based on latest feedback

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -34,22 +34,22 @@ const Landing = props => {
           <h2>Welcome to the NECCC Cover Crop Decision Support Tools</h2>
           <p>
             You are currently interacting with a Beta version of the Cover Crop
-            The purpose of this interaction is so that we may gather feedback
-            about the usability and usefulness of these tools. Technically, this
-            is called a “high-fidelity prototype”. “High fidelity”, because it’s
-            made to look and feel like the actual tool that we will build this
-            Winter. “Prototype”, because it’s meant for testing with potential
-            users of the tools.
+            DSTs. The purpose of this interaction is so that we may gather
+            feedback about the usability and usefulness of these tools. The
+            cover crop data you will see originated in the Midwest Cover Crops
+            Council (MCCC) species selector tool, the USDA Plants database, and
+            a seeding rate calculator developed by NY NRCS. It has been reviewed
+            by cover crop experts in the Northeast in each USDA plant hardiness
+            zone.{" "}
           </p>
           <p>
-            {" "}
-            The cover crop data you will see originated in the Midwest Cover
-            Crops Council (MCCC) species selector tool, the USDA Plants
-            database, and a seeding rate calculator developed by NY NRCS. It has
-            been reviewed by cover crop experts in the Northeast in each USDA
-            plant hardiness zone. Please note: these data are still being
-            finalized and are shown here as a preview.
+            Please note: these data are still being finalized and are shown here
+            as a preview. Thank you for your time and consideration. We look
+            forward to your feedback and hope to build a pleasant cover crop
+            tool experience that helps you to effectively select and manage your
+            cover crops.
           </p>
+
           <p className="mt-4">
             {" "}
             Thank you for your time and consideration. We look forward to your
