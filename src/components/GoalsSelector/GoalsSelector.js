09ad0b8897cd8997a4283f2a7a9937bb0d0eb58c
Rohit Bandooni <rbandoo@ncsu.edu>
2020-02-25 19:52:43 -0500

    goals selector : modified styles, avatar logic

--- src/components/GoalsSelector/GoalsSelector.js
+++ src/components/GoalsSelector/GoalsSelector.js
@@ -3,12 +3,31 @@ import React, { useContext, useEffect, Fragment, useState } from "react";
 import { Context } from "../../store/Store";
 
 import "../../styles/goalsSelector.scss";
-import { Tooltip, Chip, CircularProgress } from "@material-ui/core";
+import {
+  Tooltip,
+  Chip,
+  CircularProgress,
+  Badge,
+  makeStyles
+} from "@material-ui/core";
 
 import Skeleton from "@material-ui/lab/Skeleton";
+import GoalTag from "./GoalTag";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(1)
+    }
+  }
+}));
 
 const GoalsSelector = () => {
   const [state, dispatch] = useContext(Context);
+  const classes = useStyles();
 
   const goalsURL =
     "https://api.airtable.com/v0/appC47111lCOTaMYe/Cover%20Crop%20Goals?maxRecords=300";
@@ -17,33 +36,6 @@ const GoalsSelector = () => {
   useEffect(() => {
     fetchGoals();
   }, []);
-
-  const updateSelectedGoals = (item, key) => {
-    const goals = [...state.selectedGoals];
-
-    if (goals.indexOf(item.fields["Cover Crop Goal"]) === -1) {
-      // does not exist, dispatch to state
-
-      document.getElementById(`chip${key}`).classList.add("active");
-      dispatch({
-        type: "ADD_SELECTED_GOALS",
-        data: item.fields["Cover Crop Goal"]
-      });
-    } else {
-      // exists, remove it from the state and update the state
-      let index = goals.indexOf(item.fields["Cover Crop Goal"]);
-      goals.splice(index, 1);
-
-      // make it lighter on the ui
-
-      document.getElementById(`chip${key}`).classList.remove("active");
-
-      dispatch({
-        type: "UPDATE_SELECTED_GOALS",
-        data: goals
-      });
-    }
-  };
 
   const fetchGoals = async () => {
     dispatch({
@@ -76,38 +68,27 @@ const GoalsSelector = () => {
           <h1>What are your cover cropping goals</h1>
         </div>
         <div className="col-lg-12">
-          <p>Select upto three. Hover for more information</p>
+          <p>Select up to three. Hover for more information</p>
         </div>
         {state.ajaxInProgress ? (
           <div className="goals col-lg-12">
             <CircularProgress />
           </div>
         ) : (
-          <div className="goals col-lg-12">
+          <div className="goals row">
             {state.allGoals.length > 0 ? (
               state.allGoals.map((goal, key) =>
                 !goal.fields["Cover Crop Goal"].startsWith("TBD") ? (
-                  <Tooltip
-                    interactive
-                    arrow
-                    title={
-                      <div className="tooltipTextContainer">
-                        <p>{goal.fields["Description"]}</p>
-                      </div>
-                    }
-                    key={`tooltip${key}`}
-                  >
-                    <Chip
-                      disabled={false}
-                      label={goal.fields["Cover Crop Goal"].toUpperCase()}
-                      onClick={() => updateSelectedGoals(goal, key)}
-                      key={`chip${key}`}
-                      id={`chip${key}`}
-                      size="medium"
-                      variant="outlined"
-                      className="goal enabled"
+                  <div key={key} className={classes.root}>
+                    {/* //   <Badge badgeContent={4} color="primary"> */}
+                    {/* updateSelectedGoals(goal, key) */}
+                    <GoalTag
+                      goal={goal}
+                      id={key}
+                      goaltTitle={goal.fields["Cover Crop Goal"]}
+                      goalDescription={goal.fields["Description"]}
                     />
-                  </Tooltip>
+                  </div>
                 ) : (
                   ""
                 )
