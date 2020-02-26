4a3e365642b5aa844043ae58fa4510509e40dc09
Rohit Bandooni <rbandoo@ncsu.edu>
2020-02-25 20:10:36 -0500

    update styles based on caller prop

--- src/components/Location/SoilConditions.js
+++ src/components/Location/SoilConditions.js
@@ -2,7 +2,17 @@ import React from "react";
 import TerrainIcon from "@material-ui/icons/Terrain";
 import Slider from "@material-ui/core/Slider";
 
-const SoilConditions = () => {
+const SoilConditions = props => {
+  const [caller, setCaller] = React.useState("");
+
+  React.useEffect(() => {
+    if (props.caller) {
+      setCaller(props.caller);
+    } else {
+      setCaller("");
+    }
+  }, [props]);
+
   return (
     <div className="row">
       <div className="col-lg-12">
@@ -12,26 +22,47 @@ const SoilConditions = () => {
         <h6 className="font-weight-bold text-uppercase">
           <TerrainIcon /> SOILS COMPOSITION
         </h6>
-        <p className="offset-lg-1 col-lg-6">Loam (54%), Silt Loam (38%)</p>
+        {caller == "greenbar" ? (
+          <p className="col-lg-12">Loam (54%), Silt Loam (38%)</p>
+        ) : (
+          <p className="offset-lg-1 col-lg-6">Loam (54%), Silt Loam (38%)</p>
+        )}
       </div>
       <div className="col-lg-12 mt-2">
         <h6 className="font-weight-bold text-uppercase">
           <TerrainIcon style={{ color: "transparent" }} />
           DRAINAGE CLASS
         </h6>
-        <div className="drainageSlider col-lg-6 offset-lg-1">
-          <Slider
-            defaultValue={50}
-            // getAriaValueText={}
-            aria-labelledby="discrete-slider"
-            valueLabelDisplay="auto"
-            step={25}
-            marks
-            min={0}
-            max={100}
-          />
-          <p className="text-center text-uppercase">WELL DRAINED</p>
-        </div>
+
+        {caller == "greenbar" ? (
+          <div className="drainageSlider col-lg-12">
+            <Slider
+              defaultValue={50}
+              // getAriaValueText={}
+              aria-labelledby="discrete-slider"
+              valueLabelDisplay="auto"
+              step={25}
+              marks
+              min={0}
+              max={100}
+            />
+            <p className="text-center text-uppercase">WELL DRAINED</p>
+          </div>
+        ) : (
+          <div className="drainageSlider col-lg-6 offset-lg-1">
+            <Slider
+              defaultValue={50}
+              // getAriaValueText={}
+              aria-labelledby="discrete-slider"
+              valueLabelDisplay="auto"
+              step={25}
+              marks
+              min={0}
+              max={100}
+            />
+            <p className="text-center text-uppercase">WELL DRAINED</p>
+          </div>
+        )}
       </div>
       <div className="col-lg-12 mt-2">
         <h6 className="font-weight-bold text-uppercase">
