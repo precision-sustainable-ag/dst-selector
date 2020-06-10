f1a247fdcc4f33de4ea9073be4866f4536446acc
Rohit Bandooni <rbandoo@ncsu.edu>
2020-06-10 01:11:45 -0400

    hardcode filter templates

---
+++ src/components/CropSelector/Filters/Termination.js
@@ -0,0 +1,39 @@
+import React, { useState, Fragment } from "react";
+import {
+  ListItem,
+  ListItemText,
+  Collapse,
+  Checkbox,
+  Typography,
+  FormControlLabel,
+} from "@material-ui/core";
+import { ExpandLess, ExpandMore } from "@material-ui/icons";
+
+const Termination = () => {
+  const [open, setOpen] = useState({
+    0: false,
+    1: false,
+  });
+  return (
+    <Fragment>
+      <ListItem button onClick={() => setOpen({ ...open, 0: !open[0] })}>
+        <ListItemText primary="Cover Crop Group" disableTypography />
+        {open[0] ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open[0]}>
+        <FormControlLabel
+          //   classes={{ root: classes.formControlLabel }}
+          control={<Checkbox value="Legume" />}
+          label={<Typography variant="body2">Legume</Typography>}
+        />
+      </Collapse>
+      <ListItem button onClick={() => setOpen({ ...open, 1: !open[1] })}>
+        <ListItemText primary="Family Common Name" disableTypography />
+        {open[1] ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open[1]}>Family Common Name</Collapse>
+    </Fragment>
+  );
+};
+
+export default Termination;
