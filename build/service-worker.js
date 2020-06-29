761f1bb55ae6fde323a8b0fcb601d4afb92e24b2
Rohit Bandooni <rbandoo@ncsu.edu>
2020-06-29 08:26:50 -0400

    responsive changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c039166ee02669e0d70e804300baf043.js"
+  "/precache-manifest.365da3b789e14f853d8174f44a8526f3.js"
 );
 
 self.addEventListener('message', (event) => {
