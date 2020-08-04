c21da15bf480dab9f98ecb8ed98506a66696c852
Rohit Bandooni <rbandoo@ncsu.edu>
2020-08-03 23:32:31 -0400

    new components

---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const Feedback = (props) => {
+  return (
+    <div>
+      <div>Feedback</div>
+    </div>
+  );
+};
+
+export default Feedback;
