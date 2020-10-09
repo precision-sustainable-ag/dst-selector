4c26c8d93aca6b6fed535c738a72a5a07f09d727
Rohit Bandooni <rbandoo@ncsu.edu>
2020-10-09 07:16:28 -0400

    patches growth window

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a694b178e71bd5b865613817b7022d7.js"
+  "/precache-manifest.1b70bf2c96159d96a598570350809c7f.js"
 );
 
 self.addEventListener('message', (event) => {
