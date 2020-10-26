8f18dd8387cdebb74daadaaf2ba199a6d1428f5c
Rohit Bandooni <rbandoo@ncsu.edu>
2020-10-26 17:33:12 +0000

    exts

--- public/images/Cover Crop Photos/ren.sh
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-
-set "exts= "
-for %%f in (*.*) do if "!exts:%%~Xf=!" equ "!exts!" set "exts=!exts! %%~Xf"
-for %%c in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do set "exts=!exts:%%c=%%c!"
-for %%e in (%exts%) do ren "*%%e" "*%%e"
-
