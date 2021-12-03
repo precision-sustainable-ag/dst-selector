e084a94f82c52f651b0201737d1e01ca93583b5f
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2021-12-03 13:37:57 -0500

    fix merge

--- build/service-worker.js
+++ build/service-worker.js
@@ -11,18 +11,14 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
 importScripts(
-<<<<<<< HEAD
-  "/precache-manifest.0d69e4be8dcd2374ffdf17187920796c.js"
-=======
-  "/precache-manifest.a9b7b3deacbcc72564627293f5b1aa93.js"
->>>>>>> issue-#133/Displaying-units
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+importScripts("/precache-manifest.a9b7b3deacbcc72564627293f5b1aa93.js");
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
@@ -37,7 +33,9 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
-});
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL("/index.html"),
+  {
+    blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
+  }
+);
