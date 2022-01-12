e67c77349f03e90b8c37928f145d405e941cffce
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2022-01-12 09:18:13 -0500

    build

---
+++ build/precache-manifest.846bb4f5d453c9098f01b5a7eef6ccff.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7d8bbc3882e0f12004c12ac805db49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48566cc9e433c0b189d5",
+    "url": "/static/css/2.0fd5b7b5.chunk.css"
+  },
+  {
+    "revision": "120d74e6a3d273e7d416",
+    "url": "/static/css/main.d064262c.chunk.css"
+  },
+  {
+    "revision": "48566cc9e433c0b189d5",
+    "url": "/static/js/2.abcac28e.chunk.js"
+  },
+  {
+    "revision": "e6f866a2b2e0d305ef1b0e00829cecf5",
+    "url": "/static/js/2.abcac28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120d74e6a3d273e7d416",
+    "url": "/static/js/main.ddbe640b.chunk.js"
+  },
+  {
+    "revision": "376506a4570fafe0b1fd",
+    "url": "/static/js/runtime-main.518259b1.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "fd5728f2cf777b06b966d05c0c823dc9",
+    "url": "/static/media/spritesheet.fd5728f2.svg"
+  }
+]);
