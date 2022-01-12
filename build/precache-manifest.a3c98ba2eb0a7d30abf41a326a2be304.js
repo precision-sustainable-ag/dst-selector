264fd82b3b459918474109e846c9bed10f6edd19
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2022-01-12 09:17:16 -0500

    build

--- build/precache-manifest.a3c98ba2eb0a7d30abf41a326a2be304.js
+++ build/precache-manifest.a3c98ba2eb0a7d30abf41a326a2be304.js
@@ -1,42 +1,42 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    revision: "c1794bfdb470e2791f99c43223616f03",
-    url: "/index.html",
+    "revision": "c1794bfdb470e2791f99c43223616f03",
+    "url": "/index.html"
   },
   {
-    revision: "0d00425aada16ef1ca39",
-    url: "/static/css/2.0fd5b7b5.chunk.css",
+    "revision": "0d00425aada16ef1ca39",
+    "url": "/static/css/2.0fd5b7b5.chunk.css"
   },
   {
-    revision: "7f906e59b5e651a79fac",
-    url: "/static/css/main.d064262c.chunk.css",
+    "revision": "7f906e59b5e651a79fac",
+    "url": "/static/css/main.d064262c.chunk.css"
   },
   {
-    revision: "0d00425aada16ef1ca39",
-    url: "/static/js/2.dc6ca95c.chunk.js",
+    "revision": "0d00425aada16ef1ca39",
+    "url": "/static/js/2.dc6ca95c.chunk.js"
   },
   {
-    revision: "e6f866a2b2e0d305ef1b0e00829cecf5",
-    url: "/static/js/2.dc6ca95c.chunk.js.LICENSE.txt",
+    "revision": "e6f866a2b2e0d305ef1b0e00829cecf5",
+    "url": "/static/js/2.dc6ca95c.chunk.js.LICENSE.txt"
   },
   {
-    revision: "7f906e59b5e651a79fac",
-    url: "/static/js/main.d48efbef.chunk.js",
+    "revision": "7f906e59b5e651a79fac",
+    "url": "/static/js/main.d48efbef.chunk.js"
   },
   {
-    revision: "376506a4570fafe0b1fd",
-    url: "/static/js/runtime-main.518259b1.js",
+    "revision": "376506a4570fafe0b1fd",
+    "url": "/static/js/runtime-main.518259b1.js"
   },
   {
-    revision: "11911410dca2de148f30954eb2fd5eab",
-    url: "/static/media/arrow_left.11911410.svg",
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
   },
   {
-    revision: "8ef6a08cdc1154920165680a4edde771",
-    url: "/static/media/arrow_right.8ef6a08c.svg",
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
   },
   {
-    revision: "fd5728f2cf777b06b966d05c0c823dc9",
-    url: "/static/media/spritesheet.fd5728f2.svg",
-  },
+    "revision": "fd5728f2cf777b06b966d05c0c823dc9",
+    "url": "/static/media/spritesheet.fd5728f2.svg"
+  }
 ]);
