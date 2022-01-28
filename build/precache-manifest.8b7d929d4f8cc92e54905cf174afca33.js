7747e5f85b9c7a3688523ba124d929ad6b04f790
Rick Hitchcock <rickhitchcock@outlook.com>
2022-01-28 12:51:44 -0500

    build

---
+++ build/precache-manifest.8b7d929d4f8cc92e54905cf174afca33.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e1c23fedde1b0d355717308c52fe01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9d2df77097e583e579d",
+    "url": "/static/css/2.0fd5b7b5.chunk.css"
+  },
+  {
+    "revision": "a33b76a9616e4f891e56",
+    "url": "/static/css/main.6463ce6d.chunk.css"
+  },
+  {
+    "revision": "c9d2df77097e583e579d",
+    "url": "/static/js/2.43ce876c.chunk.js"
+  },
+  {
+    "revision": "5dc354645a8b3c85c85e9683b12f6533",
+    "url": "/static/js/2.43ce876c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a33b76a9616e4f891e56",
+    "url": "/static/js/main.5a4a2299.chunk.js"
+  },
+  {
+    "revision": "376506a4570fafe0b1fd",
+    "url": "/static/js/runtime-main.518259b1.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "fd5728f2cf777b06b966d05c0c823dc9",
+    "url": "/static/media/spritesheet.fd5728f2.svg"
+  }
+]);
