a478825105c0d6d17422ecb61a4055b466a1c4a6
Adam Smith <adamws33@gmail.com>
2022-08-03 09:54:39 -0700

    removed unused keys

--- src/shared/keys.js
+++ src/shared/keys.js
@@ -1,15 +1,7 @@
 const {
-  REACT_APP_GITHUB_ORG,
-  REACT_APP_GITHUB_REPO,
-  REACT_APP_GITHUB_PAT,
   REACT_APP_GOOGLE_API_KEY,
   REACT_APP_OPEN_WEATHER_API_KEY,
-  REACT_APP_GEOCODE_AUTH,
 } = process.env;
 
-export const owner = REACT_APP_GITHUB_ORG;
-export const repo = REACT_APP_GITHUB_REPO;
-export const accessToken = REACT_APP_GITHUB_PAT;
 export const googleApiKey = REACT_APP_GOOGLE_API_KEY;
 export const openWeatherApiKey = REACT_APP_OPEN_WEATHER_API_KEY;
-export const geocodeAuth = REACT_APP_GEOCODE_AUTH;
