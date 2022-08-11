6c8f1784df69680922d6fd673f2155624e760e00
Adam Smith <adamws33@gmail.com>
2022-08-11 16:38:37 -0700

    updated ternary operators in CropSidebar

--- src/store/Store.js
+++ src/store/Store.js
@@ -280,6 +280,7 @@ const Store = ({ children }) => {
   const [state, dispatch] = useReducer(Reducer, initialState);
 
   const change = (type, e, value = e && e.target.value) => {
+    
     if (typeof value === 'object') {
       dispatch({
         type,
