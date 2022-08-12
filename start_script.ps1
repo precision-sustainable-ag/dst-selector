4d8ff9872eea00b491f0d0e507157b583d30a2dd
Drew K <drewlearns@hey.com>
2022-08-12 10:27:57 -0400

    dockerized (#57)
    * dockerized
    
    * convert to powershell
    
    Co-authored-by: Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>

---
+++ start_script.ps1
@@ -0,0 +1,9 @@
+#!/usr/bin/env pwsh
+# Date Created: 08/12/2022
+# Date Last Modified: 8/12/2022
+# Description: Devops Commands template powershell - will be use to develop pipelines later but just a WIP for now
+# Usage: ./start_script.ps1
+
+docker build . -t nginx-dst-selector:latest
+docker run -p 3000:80 nginx-dst-selector:latest
+Start-Process http://localhost:3000
