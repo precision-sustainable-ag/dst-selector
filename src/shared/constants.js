31f47dea6bf953e85b95be93f953c5ef0e46e42f
Adam Smith <adamws33@gmail.com>
2022-09-26 12:36:24 -0700

    removed location from components folder

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -73,14 +73,14 @@ export const DataTooltip = ({ data, placement = 'top-start' }) => (
   </Tooltip>
 );
 
-export const locationIcon = (w, h) => (
-  <svg width={w} height={h} viewBox="0 0 14 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M7 0C3.13 0 0 3.13 0 7C0 12.25 7 20 7 20C7 20 14 12.25 14 7C14 3.13 10.87 0 7 0ZM7 9.5C5.62 9.5 4.5 8.38 4.5 7C4.5 5.62 5.62 4.5 7 4.5C8.38 4.5 9.5 5.62 9.5 7C9.5 8.38 8.38 9.5 7 9.5Z"
-      fill="white"
-    />
-  </svg>
-);
+// export const locationIcon = (w, h) => (
+//   <svg width={w} height={h} viewBox="0 0 14 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+//     <path
+//       d="M7 0C3.13 0 0 3.13 0 7C0 12.25 7 20 7 20C7 20 14 12.25 14 7C14 3.13 10.87 0 7 0ZM7 9.5C5.62 9.5 4.5 8.38 4.5 7C4.5 5.62 5.62 4.5 7 4.5C8.38 4.5 9.5 5.62 9.5 7C9.5 8.38 8.38 9.5 7 9.5Z"
+//       fill="white"
+//     />
+//   </svg>
+// );
 export const zoneIcon = (w, h) => (
   <svg height={h} width={w} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
