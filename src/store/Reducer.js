2fa98c784af0abeea81d0214ef40719f1ff4e306
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2023-01-05 13:00:59 -0500

    fix

--- src/store/Reducer.js
+++ src/store/Reducer.js
@@ -6,6 +6,7 @@
 const Reducer = (state, action, value = action && action.data && action.data.value) => {
   const section = window.location.href.includes('species-selector') ? 'selector' : 'explorer';
   let sfilters = { ...state[section] };
+  console.log(sfilters);
 
   switch (action.type) {
     case 'TOGGLE':
