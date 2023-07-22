a5451fdcd5284a6e79f46b13df20f0c10598be00
ameyachavan007 <chavanameya007@gmail.com>
2023-07-22 19:07:11 -0400

    reduxMig/refactor-address-states
    make changes related to address state slice

--- src/reduxStore/addressSlice.js
+++ src/reduxStore/addressSlice.js
@@ -1,56 +1,63 @@
 const initialState = {
-  address: '',
-  markers: [[40.78489145, -74.80733626930342]],
-  addressVerified: false,
-  fullAddress: '',
-  addressChangedViaMap: false,
-  zipCode: 0,
-  zone: '', // not initialised earlier
-  zoneText: '', // not initialised earlier
-  zoneId: null,
-  lastZipCode: 0,
-  lastZone: '',
+    address: '',
+    markers: [[40.78489145, -74.80733626930342]],
+    addressVerified: false,
+    fullAddress: '',
+    addressChangedViaMap: false,
+    zipCode: 0,
+    zone: 'hey', //not initialised earlier
+    zoneText: '', //not initialised earlier
+    zoneId: null,
+    lastZipCode: 0,
+    lastZone: '',
+    county: null,
+    
+}
 
+export const updateLocation = ({ address, markers, zipCode }) => {
+    return {
+        type: 'UPDATE_LOCATION',
+        payload: {
+            address,
+            markers,
+            zipCode,
+        },
+    };
 };
 
-export const updateLocation = ({
-  address, latitude, longitude, zipCode,
-}) => ({
-  type: 'UPDATE_LOCATION',
-  payload: {
-    address,
-    latitude,
-    longitude,
-    zipCode,
-  },
-});
+export const changeAddress = ({address, addressVerified}) => {
+    return {
+        type: 'CHANGE_ADDRESS',
+        payload: {
+            address,
+            addressVerified,
+        },
+    };
+};
 
-export const changeAddress = ({ address, addressVerified }) => ({
-  type: 'CHANGE_ADDRESS',
-  payload: {
-    address,
-    addressVerified,
-  },
-});
+export const changeAddressViaMap = ({address, fullAddress, zipCode, county, addressVerified}) => {
+    return {
+        type: 'CHANGE_ADDRESS_VIA_MAP',
+        payload: {
+            address,
+            fullAddress,
+            zipCode,
+            county,
+            addressVerified,
+        },
+    };
+};
 
-export const changeAddressViaMap = ({
-  address, fullAddress, zipCode, addressVerified,
-}) => ({
-  type: 'CHANGE_ADDRESS_VIA_MAP',
-  payload: {
-    address,
-    fullAddress,
-    zipCode,
-    addressVerified,
-  },
-});
-
-export const updateMarker = (value) => ({
-  type: 'UPDATE_MARKER',
-  payload: {
-    value,
-  },
-});
+export const updateZone = ({zoneText, zone, zoneId}) => {
+    return {
+        type: 'UPDATE_ZONE',
+        payload: {
+            zoneText,
+            zone,
+            zoneId
+        },
+    };
+};
 
 export const updateZone = ({ zoneText, zone, zoneId }) => ({
   type: 'UPDATE_ZONE',
@@ -83,31 +90,71 @@ export const updateLastZone = (value) => ({
 });
 
 const addressReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'UPDATE_LOCATION':
-      return {
-        ...state,
-        address: action.payload.address,
-        markers: [[action.payload.latitude, action.payload.longitude]],
-        zipCode: action.payload.zipCode,
-      };
+    switch (action.type) {
+        case 'UPDATE_LOCATION':
+            console.log("UPDATE_LOCATION");
+            return {
+              ...state,
+            //   lastZipCode: state.zipCode, 
+              address: action.payload.address,
+              markers: action.payload.markers,
+              zipCode: action.payload.zipCode ,
+            };
 
-    case 'CHANGE_ADDRESS':
-      return {
-        ...state,
-        address: action.payload.address,
-        addressVerified: action.payload.addressVerified,
-      };
-
-    case 'CHANGE_ADDRESS_VIA_MAP':
-      return {
-        ...state,
-        address: action.payload.address,
-        fullAddress: action.payload.fullAddress,
-        zipCode: action.payload.zipCode,
-        addressVerified: action.payload.addressVerified,
-        addressChangedViaMap: true,
-      };
+        case 'CHANGE_ADDRESS':
+            console.log('CHANGE_ADDRESS');
+            return {
+                ...state,
+                address: action.payload.address,
+                addressVerified: action.payload.addressVerified,
+            };
+        
+        case 'CHANGE_ADDRESS_VIA_MAP':
+            console.log('CHANGE_ADDRESS_VIA_MAP');
+            return {
+                ...state,
+                address: action.payload.address,
+                fullAddress: action.payload.fullAddress,
+                zipCode: action.payload.zipCode,
+                county: action.payload.county,
+                addressVerified: action.payload.addressVerified,
+                addressChangedViaMap: true,
+            };
+        
+        case 'UPDATE_ZONE':
+            console.log( 'UPDATE_ZONE');
+            return {
+                ...state,
+                zoneText: action.payload.zoneText,
+                zone: action.payload.zone,
+                zoneId: action.payload.zoneId,
+            };
+        
+        case 'UPDATE_ZIP_CODE':
+            console.log('UPDATE_ZIP_CODE');
+            return {
+                ...state,
+                zipCode: action.payload.value,
+            };
+        
+        case 'LAST_ZIP_CODE':
+            console.log('LAST_ZIP_CODE');
+            return {
+                ...state,
+                lastZipCode: action.payload.value,
+            };
+        
+        case 'UPDATE_LAST_ZONE':
+            console.log('UPDATE_LAST_ZONE');
+            return {
+                ...state,
+                lastZone: action.payload.value,
+            };
+            
+        default:
+            return {...state};
+    }
+}
 
     case 'UPDATE_MARKER':
       return {
