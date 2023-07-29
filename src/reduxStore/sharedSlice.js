ed97041ca9c7348f461400bc700d397f811d384a
ameyachavan007 <chavanameya007@gmail.com>
2023-07-29 10:26:08 -0400

    created redux store
    redux-store implementation

---
+++ src/reduxStore/sharedSlice.js
@@ -0,0 +1,187 @@
+const initialState = {
+    progress: 0,
+    consent: false,
+    snackOpen: false,
+    snackMessage: '',
+    ajaxInProgress: false,
+    myCoverCropActivationFlag: false,
+    speciesSelectorActivationFlag: true,
+    comparisonKeys: [],
+    myCoverCropListLocation: '',
+    snackVertical: 'bottom',
+    snackHorizontal: 'right',
+}
+
+export const toggleValue = (value) => {
+    console.log("action creator", value);
+    return {
+        type: 'TOGGLE',
+        payload: {
+            value,
+        },
+    };
+};
+
+export const updateProgress = (value) => {
+    return {
+        type: 'UPDATE_PROGRESS',
+        payload: {
+            value,
+        },
+    };
+};
+
+export const updateConsent = (value) => {
+    return {
+        type: 'UPDATE_CONSENT',
+        payload: {
+            value,
+        },
+    };
+};
+
+export const gotoProgress = (value) => {
+    return {
+        type: 'GOTO_PROGRESS',
+        payload: {
+            value,
+        },
+    };
+};
+
+export const snackHandler = ({snackOpen, snackMessage}) => {
+    return {
+        type: 'SNACK',
+        payload: {
+            snackOpen,
+            snackMessage,
+        },
+    };
+};
+
+export const setAjaxInProgress = (value) => {
+    return {
+        type: 'SET_AJAX_IN_PROGRESS',
+        payload: {
+            value,
+        },
+    };
+};
+
+export const updateComparisonKeys = (value) => {
+    return {
+        type: 'UPDATE_COMPARISON_KEYS',
+        payload: {
+            value,
+        },
+    };
+};
+
+export const activateMyCoverCropListTile = ({myCoverCropActivationFlag, speciesSelectorActivationFlag}) => {
+    return {
+        type: 'ACTIVATE_MY_COVER_CROP_LIST_TILE',
+        payload:{
+            myCoverCropActivationFlag,
+            speciesSelectorActivationFlag,
+        },
+    };
+};
+
+export const activateSpeicesSelectorTile = ({myCoverCropActivationFlag, speciesSelectorActivationFlag}) => {
+    return {
+        type: 'ACTIVATE_SPECIES_SELECTOR_TILE',
+        payload:{
+            myCoverCropActivationFlag,
+            speciesSelectorActivationFlag,
+        },
+    };
+};
+
+export const myCropListLocation = ({from}) => {
+    return {
+        type: 'MY_CROP_LIST_LOCATION',
+        payload: {
+            from,
+        }
+    }
+}
+
+const sharedReducer = (state = initialState, action) => {
+    const value = action && action.payload && action.payload.value;
+    switch (action.type) {
+        case 'TOGGLE':
+            return {
+                ...state,
+                [value]: !state[value]
+            };
+
+        case 'UPDATE_PROGRESS':
+            if (action.payload.value === 'INCREMENT') {
+                return {...state, progress: state.progress + 1};
+            }
+            else if (action.payload.value === 'DECREMENT') {
+                return {...state, progress: state.progress - 1};
+            }
+            else if (action.payload.value === 'HOME') {
+                return {...state, progress: 0};
+            }
+
+        case 'UPDATE_CONSENT':
+            if(action.payload.value === true) {
+                return {...state, consent: true};
+            }
+            else if (action.payload.value === false) {
+                return {...state, consent: false};
+            }
+
+        case 'GOTO_PROGRESS':
+            return {
+                ...state,
+                progress: action.payload.value,
+            };
+
+        case 'SNACK':
+            return {
+                ...state,
+                snackOpen: action.payload.snackOpen,
+                snackMessage: action.payload.snackMessage,
+            };
+
+        case 'SET_AJAX_IN_PROGRESS':
+            return {
+                ...state,
+                ajaxInProgress: action.payload.value,
+            };
+
+        case 'ACTIVATE_MY_COVER_CROP_LIST_TILE': 
+            return {
+                ...state,
+                myCoverCropActivationFlag: action.payload.myCoverCropActivationFlag,
+                speciesSelectorActivationFlag: action.payload.speciesSelectorActivationFlag,
+            };
+
+        case 'ACTIVATE_SPECIES_SELECTOR_TILE': 
+            return {
+                ...state,
+                myCoverCropActivationFlag: action.payload.myCoverCropActivationFlag,
+                speciesSelectorActivationFlag: action.payload.speciesSelectorActivationFlag,
+            };
+
+        case 'UPDATE_COMPARISON_KEYS':
+            return {
+                ...state,
+                comparisonKeys: action.payload.value,
+            };
+
+        case 'MY_CROP_LIST_LOCATION':
+            return {
+                ...state,
+                myCoverCropListLocation: action.payload.from,
+            };
+
+        default:
+            return { ...state };
+    };
+};
+
+export default sharedReducer;
---
+++ src/reduxStore/soilSlice.js
@@ -0,0 +1,128 @@
+const initialState = {
+    soilData: {
+        Map_Unit_Name: '',
+        Drainage_Class: [],
+        Flooding_Frequency: [],
+        Ponding_Frequency: '',
+      },
+      soilDataOriginal: {
+        Map_Unit_Name: '',
+        Drainage_Class: [],
+        Flooding_Frequency: [],
+        Ponding_Frequency: '',
+      },
+      isSoilDataLoading: false,
+};
+
+export const updateSoilData = ({ Map_Unit_Name, Drainage_Class, Flooding_Frequency, PondingFrequency, latLong }) => {
+  return {
+    type: 'UPDATE_SOIL_DATA',
+    payload: {
+      mapUnitName: Map_Unit_Name,
+      drainageClass: Drainage_Class,
+      floodingFrequency: Flooding_Frequency,
+      pondingFrequency: PondingFrequency,
+      latLong,
+    },
+  };
+};
+
+export const updateSoilDataOriginal = ({ Map_Unit_Name, Drainage_Class, Flooding_Frequency, PondingFrequency, latLong }) => {
+  return {
+    type: 'UPDATE_SOIL_DATA_ORIGINAL',
+    payload: {
+      mapUnitName: Map_Unit_Name,
+      drainageClass: Drainage_Class,
+      floodingFrequency: Flooding_Frequency,
+      pondingFrequency: PondingFrequency,
+      latLong,
+    },
+  };
+};
+
+export const toggleSoilLoader = (value) => {
+  return {
+    type: 'TOGGLE_SOIL_LOADER',
+    payload: {
+      value,
+    },
+  };
+};
+
+export const updatedDrainageClass = (value) => {
+  return {
+    type: 'UPDATE_DRAINAGE_CLASS',
+    payload: {
+      value,
+    },
+  };
+};
+
+export const updatedFloodingFrequency = (value) => {
+  return {
+    type: 'UPDATE_FLOODING_FREQUENCY',
+    payload: {
+      value,
+    },
+  };
+};
+
+const soilReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'UPDATE_SOIL_DATA':
+      return {
+        ...state,
+        soilData: {
+          ...state.soilData,
+          Map_Unit_Name: action.payload.mapUnitName,
+          Drainage_Class: action.payload.drainageClass,
+          Flooding_Frequency: action.payload.floodingFrequency,
+          Ponding_Frequency: action.payload.pondingFrequency,
+          for: action.payload.latLong
+        },
+      };
+
+    // why are there two similar reducers?
+    case 'UPDATE_SOIL_DATA_ORIGINAL':
+      return {
+        ...state,
+        soilData: {
+          ...state.soilData,
+          Map_Unit_Name: action.payload.mapUnitName,
+          Drainage_Class: action.payload.drainageClass,
+          Flooding_Frequency: action.payload.floodingFrequency,
+          Ponding_Frequency: action.payload.pondingFrequency,
+          for: action.payload.latLong
+        },
+      };
+
+    case 'TOGGLE_SOIL_LOADER':
+      return {
+        ...state,
+        isSoilDataLoading: action.payload.value,
+      };
+
+    case 'UPDATE_DRAINAGE_CLASS':
+      return {
+        ...state,
+        soilData: {
+          ...state.soilData,
+          Drainage_Class: action.payload.value,
+        },
+      };
+
+    case 'UPDATE_FLOODING_FREQUENCY':
+      return {
+        ...state,
+        soilData: {
+          ...state.soilData,
+          Flooding_Frequency: action.payload.value,
+        },
+      };
+
+    default:
+      return { ...state }
+  }
+}
+
+export default soilReducer;
