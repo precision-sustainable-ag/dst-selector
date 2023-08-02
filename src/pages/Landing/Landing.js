b1b33241366b9474681e4f83ced2fe1db7db2b1c
Adam Smith <adamws33@gmail.com>
2023-08-02 16:13:00 -0700

    updated state.state to hold a string and not an object, updated looping when map selection is made then going back to location component

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -115,7 +115,7 @@ const Landing = ({ height, title, bg }) => {
     dispatch({
       type: 'UPDATE_STATE',
       data: {
-        state: selState,
+        state: selState.label,
         stateId: selState.id,
         councilShorthand: selState.council.shorthand,
         councilLabel: selState.council.label,
