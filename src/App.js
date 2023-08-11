68fe94d5c5a4f671a51f526eea837938454716d8
ssharm1524 <ssharma18s5@gmail.com>
2023-08-11 09:37:10 -0700

    fixed borders issue

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ const App = () => {
     <Box className="contentWrapper" id="mainContentWrapper">
       <Header logo="neccc_wide_logo_color_web.jpg" />
 
-      <Container maxWidth={false}>
+      <Container disableGutters maxWidth={false}>
         <Box className="contentContainer">
           {state.progress === 0 ? (
             <Landing
