1ec8476adeaa6f3177cb71335ee1bfdb2f8f032a
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2023-08-17 10:09:30 -0400

    cleanup logic

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import {
 } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 import CropSelector from './pages/CropSelector/CropSelector';
 import GoalsSelector from './pages/GoalsSelector/GoalsSelector';
 import Header from './pages/Header/Header';
@@ -17,7 +16,6 @@ import LocationComponent from './pages/Location/Location';
 import LocationConfirmation from './pages/Location/LocationConfirmation/LocationConfirmation';
 import './styles/App.scss';
 import { snackHandler } from './reduxStore/sharedSlice';
-import { updateRegion, updateStateInfo } from './reduxStore/mapSlice';
 
 const LoadRelevantRoute = ({ progress, calcHeight }) => {
   switch (progress) {
@@ -59,7 +57,6 @@ const LoadRelevantRoute = ({ progress, calcHeight }) => {
 const App = () => {
   const dispatchRedux = useDispatch();
   const [calcHeight, setCalcHeight] = useState(0);
-  const history = useHistory();
 
   // redux vars
   const snackOpenRedux = useSelector((stateRedux) => stateRedux.sharedData.snackOpen);
@@ -68,38 +65,8 @@ const App = () => {
   const snackVerticalRedux = useSelector((stateRedux) => stateRedux.sharedData.snackVertical);
   const snackHorizontalRedux = useSelector((stateRedux) => stateRedux.sharedData.snackHorizontal);
 
-  // eslint-disable-next-line
-  const crop = window.location.search.match(/crop=([^\^]+)/);
-  // eslint-disable-next-line
-  const stateId = window.location.search.match(/state=([^\^]+)/); // for automating Information Sheet PDFs
-  // eslint-disable-next-line
-  const regionId = window.location.search.match(/region=([^\^]+)/); // for automating Information Sheet PDFs
-
-  // this handlespopulating redux state if there are url params
-  if (crop && stateId && regionId) {
-    dispatchRedux(updateStateInfo({
-      stateLabel: null,
-      stateId: stateId[1],
-      councilShorthand: null,
-      councilLabel: null,
-    }));
-    dispatchRedux(updateRegion({
-      regionId: regionId[1],
-      regionLabel: null,
-      regionShorthand: null,
-    }));
-    history.push(`/explorer?crop=${crop[1]}^&stateId=${stateId[1]}^&regionId=${regionId[1]}`);
-  }
-
   const handleSnackClose = () => {
     dispatchRedux(snackHandler({ snackOpen: false, snackMessage: '' }));
-    // dispatch({
-    //   type: 'SNACK',
-    //   data: {
-    //     snackOpen: false,
-    //     snackMessage: '',
-    //   },
-    // });
   };
 
   useEffect(() => {
