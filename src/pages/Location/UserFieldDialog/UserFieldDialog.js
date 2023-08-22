cfb3e3a5e0a9329be39742419ca3d11fc865d33e
YazidZidane <xjt980408@gmail.com>
2023-08-22 14:05:49 -0400

    add update feature

--- src/pages/Location/UserFieldDialog/UserFieldDialog.js
+++ src/pages/Location/UserFieldDialog/UserFieldDialog.js
@@ -10,24 +10,38 @@ import React from 'react';
 
 const UserFieldDialog = ({ fieldDialogState, setFieldDialogState, handleClose }) => {
   const {
-    open, fieldName, error, errorText,
+    open, fieldName, error, errorText, type,
   } = fieldDialogState;
   return (
     <Dialog sx={{ m: 0, p: 2 }} open={open}>
-      <DialogTitle>
-        You need to give this field a nickname
-      </DialogTitle>
-      <DialogContent>
-        <TextField
-          autoFocus
-          error={error}
-          helperText={errorText}
-          variant="standard"
-          label="Field Nickname"
-          value={fieldName}
-          onChange={(e) => setFieldDialogState({ ...fieldDialogState, fieldName: e.target.value })}
-        />
-      </DialogContent>
+      {type === 'add' && (
+      <>
+        <DialogTitle>
+          You need to give this field a nickname
+        </DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            error={error}
+            helperText={errorText}
+            variant="standard"
+            label="Field Nickname"
+            value={fieldName}
+            onChange={(e) => setFieldDialogState({ ...fieldDialogState, fieldName: e.target.value })}
+          />
+        </DialogContent>
+      </>
+      )}
+      {type === 'update' && (
+        <DialogTitle>
+          Are you going to update this field?
+        </DialogTitle>
+      )}
+      {type === 'delete' && (
+        <DialogTitle>
+          Are you going to delete this field?
+        </DialogTitle>
+      )}
       <DialogActions>
         <Button onClick={() => handleClose(true)}>OK</Button>
         <Button onClick={() => handleClose(false)}>Cancel</Button>
