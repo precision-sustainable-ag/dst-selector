acb58095dc188bbde2e796e5ba9951d530630b75
YazidZidane <xjt980408@gmail.com>
2023-08-30 11:33:21 -0400

    delete authenticate in UserFieldList

--- src/pages/Location/UserFieldList/UserFieldList.js
+++ src/pages/Location/UserFieldList/UserFieldList.js
@@ -9,26 +9,20 @@ import {
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
 
 const UserFieldList = ({
   userFields, field, setField, setFieldDialogState,
 }) => {
-  const { isAuthenticated } = useAuth0();
   const handleChange = (e) => {
     setField(e.target.value);
   };
   const handleEdit = () => {
-    if (isAuthenticated) {
-      setFieldDialogState((prev) => ({
-        ...prev, open: true, actionType: 'updateName', fieldName: field, prevName: field,
-      }));
-    }
+    setFieldDialogState((prev) => ({
+      ...prev, open: true, actionType: 'updateName', fieldName: field, prevName: field,
+    }));
   };
   const handleDelete = () => {
-    if (isAuthenticated) {
-      setFieldDialogState((prev) => ({ ...prev, open: true, actionType: 'delete' }));
-    }
+    setFieldDialogState((prev) => ({ ...prev, open: true, actionType: 'delete' }));
   };
   return (
     <List component="div" disablePadding>
