d8a4dfe8f86eb7cf228b9985c8187b07fd83c47c
YazidZidane <xjt980408@gmail.com>
2023-08-30 12:08:21 -0400

    delete logout redux logic

--- src/reduxStore/userSlice.js
+++ src/reduxStore/userSlice.js
@@ -3,10 +3,6 @@ const initialState = {
   field: null,
 };
 
-export const userLogout = () => ({
-  type: 'USER_LOGOUT',
-});
-
 export const updateAccessToken = (token) => ({
   type: 'UPDATE_ACCESSTOKEN',
   payload: { token },
@@ -19,8 +15,6 @@ export const updateField = (field) => ({
 
 const userReducer = (state = initialState, action = null) => {
   switch (action.type) {
-    case 'USER_LOGOUT':
-      return { ...initialState };
     case 'UPDATE_ACCESSTOKEN':
       return { ...state, accessToken: action.payload.token };
     case 'UPDATE_FIELD':
