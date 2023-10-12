c502e3cebf46c7b70af29baf9f8f2377bcd78c30
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2023-10-12 16:14:53 -0400

    add cover crop group and average goal rating to comparison

--- src/pages/MyCoverCropList/MyCoverCropComparison/MyCoverCropComparisonTable.js
+++ src/pages/MyCoverCropList/MyCoverCropComparison/MyCoverCropComparisonTable.js
@@ -22,35 +22,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
   DataTooltip,
   RenderSeedPriceIcons,
-  callCoverCropApi,
   getRating,
 } from '../../../shared/constants';
 import CropDetailsModal from '../../../components/CropDetailsModal/CropDetailsModal';
 import CropCard from '../../../components/CropCard/CropCard';
-import { setAjaxInProgress } from '../../../reduxStore/sharedSlice';
-
-const lightBG = {
-  border: '1px solid white',
-  backgroundColor: '#f1f7eb',
-  padding: '5px',
-  marginBottom: '5px',
-  textAlign: 'center',
-  display: 'flex',
-  justifyContent: 'center',
-  fontWeight: 'bold',
-  minHeight: '36px',
-};
-
-const GetAverageGoalRating = ({ crop }) => {
-  const selectedGoalsRedux = useSelector((stateRedux) => stateRedux.goalsData.selectedGoals);
-  let goalRating = 0;
-  selectedGoalsRedux.forEach((goal) => {
-    if (crop.data.Goals[goal]) {
-      goalRating = +crop.data.Goals[goal].values[0] + goalRating;
-    }
-  });
-  return getRating(goalRating / selectedGoalsRedux.length);
-};
 
 const MyCoverCropComparison = () => {
   const dispatchRedux = useDispatch();
@@ -61,68 +36,70 @@ const MyCoverCropComparison = () => {
   const selectedGoalsRedux = useSelector((stateRedux) => stateRedux.goalsData.selectedGoals);
   const comparisonKeysRedux = useSelector((stateRedux) => stateRedux.sharedData.comparisonKeys);
   const selectedCropsRedux = useSelector((stateRedux) => stateRedux.cropData.selectedCrops);
-  const regionIdRedux = useSelector((stateRedux) => stateRedux.mapData.regionId);
-  const stateIdRedux = useSelector((stateRedux) => stateRedux.mapData.stateId);
-  const zoneRedux = useSelector((stateRedux) => stateRedux.addressData.zone);
-  const apiBaseUrlRedux = useSelector((stateRedux) => stateRedux.sharedData.apiBaseUrl);
 
   // useState vars
-  const [loading, setLoading] = useState(false);
-  const [sidebarDefs, setSidebarDefs] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
   const [modalData, setModalData] = useState({});
-  const [dataDictionary, setDataDictionary] = useState([]);
   const [selectedCrops, setSelectedCrops] = useState([]);
+  const [rows, setRows] = useState([]);
+  const tempRows = [];
 
   // TODO: Update SelectedCropsRedux
-
-  const allData = [];
-  const query = `${encodeURIComponent('regions')}=${encodeURIComponent(regionIdRedux)}`;
-
-  // selectedCrops = selectedCrops || selectedCropsRedux;
-
-  useEffect(() => {
-    if (stateIdRedux && regionIdRedux) {
-      dispatchRedux(setAjaxInProgress(true));
-
-      setLoading(true);
-      callCoverCropApi(`https://${apiBaseUrlRedux}.covercrop-selector.org/v1/states/${stateIdRedux}/dictionary?${query}`).then((data) => {
-        setDataDictionary(data.data);
-      });
-    }
-  }, [stateIdRedux, regionIdRedux]);
-
-  const setDataDict = async () => {
-    await dataDictionary.forEach((item) => {
-      item.attributes.map((i) => allData.push(i));
-    });
-  };
-
-  useEffect(() => {
-    setDataDict()
-      .then(() => { setLoading(false); })
-      .catch((err) => {
-      // eslint-disable-next-line no-console
-        console.log(err.message);
-      });
-    setSidebarDefs(allData);
-  }, [zoneRedux, dataDictionary]);
 
   useEffect(() => {
     setSelectedCrops(cropDataRedux.filter((crop) => activeCropDataRedux.includes(crop.id)).filter((crop) => selectedCropsRedux.includes(crop.id)));
   }, [cropDataRedux, activeCropDataRedux, selectedCropsRedux]);
 
-  const [rows, setRows] = useState([]);
-  const tempRows = [];
+  const handleModalOpen = (crop) => {
+    // put data inside modal
+    setModalData(crop);
+    setModalOpen(true);
+  };
 
   const buildTable = async () => {
+    // used to add cover crop group
+    const groupRow = { comparisonKey: 'Cover Crop Group' };
+    // used to add average goal rating
+    const goalRow = { comparisonKey: 'Average Goal Rating' };
+
+    // iterate over each crop and get group and average goal rating if selectedgoals.length > 0
+    selectedCrops.forEach((crop, index) => {
+      // this is needed to format it like the other attributes
+      groupRow[`crop${index}`] = { values: [] };
+      // same thing here but it also specifies it should be a pillbox
+      goalRow[`crop${index}`] = { values: [], dataType: 'pillbox' };
+      // push crop group
+      groupRow[`crop${index}`].values.push(crop.group);
+
+      // if selected goals > 1 calculate average goal rating
+      if (selectedGoalsRedux.length > 0) {
+        let goalRating = 0;
+        selectedGoalsRedux.forEach((goal) => {
+          if (crop.data.Goals[goal]) {
+            goalRating = +crop.data.Goals[goal].values[0] + goalRating;
+          }
+        });
+
+        // push average goal rating
+        goalRow[`crop${index}`].values.push(goalRating / selectedGoalsRedux.length);
+      }
+    });
+
+    // push group
+    tempRows.push(groupRow);
+
+    // if selected goals > 1 push average goal rating
+    if (selectedGoalsRedux.length > 0) {
+      tempRows.push(goalRow);
+    }
+
     await comparisonKeysRedux.forEach((key) => {
       const newRow = { comparisonKey: key };
 
       selectedCrops.forEach((crop, index) => {
         // iterate ove all crop.data keys
         Object.keys(crop.data).forEach((dataKey) => {
-        // iterate over all crop.data.dataKey keys
+          // iterate over all crop.data.dataKey keys
           Object.keys(crop?.data?.[dataKey]).forEach((nestedKey) => {
             if (crop.data[dataKey][nestedKey]?.label === key) {
               newRow[`crop${index}`] = crop.data[dataKey][nestedKey];
@@ -136,13 +113,13 @@ const MyCoverCropComparison = () => {
   };
 
   const buildTableHeaders = () => selectedCrops.map((crop, index) => (
-    <TableCell align="center">
+    <TableCell key={index} align="center">
       <CropCard
         crop={crop}
         index={index}
         type="cropList"
-        lightBG={lightBG}
         dispatchRedux={dispatchRedux}
+        handleModalOpen={handleModalOpen}
       />
       <CropDetailsModal
         modalOpen={modalOpen}
@@ -153,13 +130,14 @@ const MyCoverCropComparison = () => {
 
   ));
 
-  const buildTableRows = (row) => Object.keys(row).map((key, i) => {
-    const attribute = row[`crop${i - 1}`];
+  const buildTableRows = (row) => Object.keys(row).map((key, index) => {
+    const attribute = row[`crop${index - 1}`];
 
     // handles the key name
     if (key === 'comparisonKey') {
       return (
         <TableCell
+          key={index}
           component="th"
           scope="row"
           sx={{
@@ -181,7 +159,7 @@ const MyCoverCropComparison = () => {
     // handles no data
     if (!attribute) {
       return (
-        <TableCell align="center">
+        <TableCell key={index} align="center">
           <Typography variant="body2">No Data</Typography>
         </TableCell>
 
@@ -226,29 +204,29 @@ const MyCoverCropComparison = () => {
   });
 
   useEffect(() => {
-    buildTable().then(() => { console.log(tempRows); setRows(tempRows); });
+    buildTable().then(() => { setRows(tempRows); });
   }, [comparisonKeysRedux, selectedCrops]);
 
-  return (!loading) && (
-  <TableContainer style={{ overflowX: 'initial' }}>
-    <Table stickyHeader aria-label="sticky table">
-      <TableHead>
-        <TableRow>
-          <TableCell />
-          {buildTableHeaders()}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {rows.map((row) => (
-          <TableRow
-            key={row.name}
-          >
-            {buildTableRows(row)}
+  return (
+    <TableContainer style={{ overflowX: 'initial' }}>
+      <Table stickyHeader aria-label="sticky table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            {buildTableHeaders()}
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </TableContainer>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+            >
+              {buildTableRows(row)}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
