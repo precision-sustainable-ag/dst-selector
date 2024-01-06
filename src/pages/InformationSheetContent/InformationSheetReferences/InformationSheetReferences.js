e0f3e93122a021f34cc8f423e0896248c1ca670b
ameyachavan007 <chavanameya007@gmail.com>
2024-01-06 19:09:08 +0530

    removed bootstrap
    remove bootstrap implementation and unnecessary styling files for information sheet references

--- src/pages/InformationSheetContent/InformationSheetReferences/InformationSheetReferences.js
+++ src/pages/InformationSheetContent/InformationSheetReferences/InformationSheetReferences.js
@@ -1,55 +1,47 @@
 import React, { Fragment } from 'react';
-import { AccordionDetails, Typography } from '@mui/material';
+import {
+  AccordionDetails,
+  Typography,
+  Box,
+  Accordion,
+  AccordionSummary,
+  Divider,
+} from '@mui/material';
 import { ExpandMore } from '@mui/icons-material';
-import { Accordion, AccordionSummary } from '../informationSheet.styles';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+// import { Accordion, AccordionSummary } from '../informationSheet.styles';
 
-const InformationSheetReferences = ({ currentSources }) => (
-  currentSources.length > 0 && (
-    <div className="row otherRows mb-4 avoidPage">
-      <div className="col-12 basicAgWrapper">
-        <div className="col-12 otherHeaderRow p-0" style={{ marginTop: '1em' }}>
-          <Accordion defaultExpanded style={{ border: '1px solid #2b7b79' }}>
-            <AccordionSummary
-              expandIcon={<ExpandMore />}
-              sx={{
-                '&$expanded': {
-                  margin: '4px 0',
-                },
-              }}
-            >
-              <Typography variant="h6" className="px-3 py-2" style={{ border: '0px' }}>
-                References & Resources
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography variant="body1" className="p-3">
-                {currentSources.length > 0
-                && currentSources.map((source, index) => (
-                  <Fragment key={index}>
-                    <a
-                      style={{
-                        textDecoration: 'underline',
-                        color: 'black',
-                        fontWeight: 'bolder',
-                      }}
-                      href={source.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {source.label}
-                    </a>
-                    {': '}
-                    {source.source}
-                    <br />
-                  </Fragment>
-                ))}
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </div>
-      </div>
-    </div>
-  )
+const InformationSheetReferences = ({ currentSources }) => currentSources.length > 0 && (
+<Box component="div" sx={{ width: '100%', m: '0.5rem' }}>
+  <Accordion defaultExpanded sx={{ border: '1px solid #2b7b79', boxShadow: 'none' }}>
+    <AccordionSummary expandIcon={<ExpandMore />}>
+      <Typography variant="h6">References & Resources</Typography>
+    </AccordionSummary>
+    <Divider sx={{ bgcolor: '#2b7b79' }} />
+    <AccordionDetails>
+      <Typography>
+        {currentSources.length > 0
+              && currentSources.map((source, index) => (
+                <Fragment key={index} sx={{ mt: '0.5rem' }}>
+                  <a
+                    style={{ textDecoration: 'underline', fontWeight: '500' }}
+                    href={source.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <OpenInNewIcon fontSize="0.9rem" />
+                    {source.label}
+                  </a>
+                  {' '}
+                  {': '}
+                  {source.source}
+                  <br />
+                </Fragment>
+              ))}
+      </Typography>
+    </AccordionDetails>
+  </Accordion>
+</Box>
 );
 
 export default InformationSheetReferences;
