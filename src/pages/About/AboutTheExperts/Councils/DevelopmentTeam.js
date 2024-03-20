833f48d17113096dc369bb72033d353059021a48
Adam Smith <adamws33@gmail.com>
2024-03-20 09:42:56 -0700

    updated about the experts to include links and a development team section

---
+++ src/pages/About/AboutTheExperts/Councils/DevelopmentTeam.js
@@ -0,0 +1,79 @@
+/*
+  This file contains the HelpComponent, helper functions, and styles
+  The HelpComponent is a static  help page that has FAQ, how to use, data dictionary, and information sheets
+  RenderContent contains all the text listed in the about section
+  styled using CustomStyles from ../../shared/constants
+*/
+
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const NortheastCoverCropExperts = () => (
+  <Typography style={{ paddingTop: '15px' }} variant="body1" align="left">
+    <p>
+      <strong>Steven Mirsky</strong>
+      <span>, USDA-ARS</span>
+    </p>
+    <p>
+      <strong>Chris Reberg-Horton</strong>
+      <span>, North Carolina State University</span>
+    </p>
+    <p>
+      <strong>Rohit Bandooni</strong>
+      <span>, North Carolina State University</span>
+    </p>
+    <p>
+      <strong>Ankita Raturi</strong>
+      <span>, Purdue University</span>
+    </p>
+    <p>
+      <strong>Juliet Norton</strong>
+      <span>, Purdue University</span>
+    </p>
+    <p>
+      <strong>Anna Morrow</strong>
+      <span>, Purdue University</span>
+    </p>
+    <p>
+      <strong>Victoria Ackroyd</strong>
+      <span>, USDA-ARS</span>
+    </p>
+    <p>
+      <strong>Brian Davis</strong>
+      <span>, North Carolina State University</span>
+    </p>
+    <p>
+      <strong>Mikah Pinegar</strong>
+      <span>, North Carolina State University</span>
+    </p>
+    <p>
+      <strong>Rick Hitchcock</strong>
+      <span>, University of Georgia</span>
+    </p>
+    <p>
+      <strong>Adam Smith</strong>
+      <span>, North Carolina State University</span>
+    </p>
+    <p>
+      <strong>Trevor Puckett</strong>
+      <span>, North Carolina State University</span>
+    </p>
+    <p>
+      <strong>Milad Agamohammadnia</strong>
+      <span>, North Carolina State University</span>
+    </p>
+    <p>
+      <strong>Jingtong Xu</strong>
+      <span>, Web Development Team</span>
+    </p>
+    <p>
+      <strong>Vyshnavi Adusumelli</strong>
+      <span>, North Carolina State University</span>
+    </p>
+    <p>
+      <strong>Heather Darby</strong>
+      <span>, University of Vermont</span>
+    </p>
+  </Typography>
+);
+export default NortheastCoverCropExperts;
---
+++ src/pages/About/AboutTheExperts/Councils/SouthernCoverCropExperts.js
@@ -0,0 +1,19 @@
+/*
+  This file contains the HelpComponent, helper functions, and styles
+  The HelpComponent is a static  help page that has FAQ, how to use, data dictionary, and information sheets
+  RenderContent contains all the text listed in the about section
+  styled using CustomStyles from ../../shared/constants
+*/
+
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const NortheastCoverCropExperts = () => (
+  <>
+    <Typography style={{ paddingTop: '15px' }} variant="body1" align="left">
+      Southern Experts will be displayed soon.
+    </Typography>
+    <br />
+  </>
+);
+export default NortheastCoverCropExperts;
