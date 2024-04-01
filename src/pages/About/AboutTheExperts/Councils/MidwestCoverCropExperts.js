15689546f15f8f9bb2bd1b9f5142053a3c2f2ae6
Adam Smith <adamws33@gmail.com>
2024-04-01 12:35:45 -0700

    updated midwest to have state/province

--- src/pages/About/AboutTheExperts/Councils/MidwestCoverCropExperts.js
+++ src/pages/About/AboutTheExperts/Councils/MidwestCoverCropExperts.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /*
   This file contains the HelpComponent, helper functions, and styles
   The HelpComponent is a static  help page that has FAQ, how to use, data dictionary, and information sheets
@@ -11,7 +12,7 @@ import React from 'react';
 const NortheastCoverCropExperts = () => (
   <>
     <Typography style={{ paddingTop: '15px' }} variant="body1" align="left">
-      The MCCC verifies cover crop data at the state level with cover crop experts from diverse state geographies and a breadth of experience.
+      The MCCC verifies cover crop data at the state/provence level with cover crop experts from diverse state geographies and a breadth of experience.
       These experts include University Extension, Government Agencies, seed industry, and farmers.
     </Typography>
     <br />
