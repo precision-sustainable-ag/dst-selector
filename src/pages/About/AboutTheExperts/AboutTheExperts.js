12792ef4682dca3bfc44779dea9d4ee400661845
Adam Smith <adamws33@gmail.com>
2024-04-05 13:48:21 -0700

    updated expertGroups object to include dataType

--- src/pages/About/AboutTheExperts/AboutTheExperts.js
+++ src/pages/About/AboutTheExperts/AboutTheExperts.js
@@ -16,13 +16,13 @@ import { getExpertsData } from '../../../shared/constants';
 const AboutTheExperts = () => {
   const [value, setValue] = useState(0);
 
-  const councils = [
-    { id: 0, menuOption: 'Development Team' },
-    { id: 1, menuOption: 'Testing Team' },
-    { id: 2, menuOption: 'Midwest Cover Crops Council' },
-    { id: 3, menuOption: 'Northeast Cover Crops Council' },
-    { id: 4, menuOption: 'Southern Cover Crops Council' },
-    { id: 5, menuOption: 'Western Cover Crops Council' },
+  const expertGroups = [
+    { id: 0, menuOption: 'Development Team', dataType: 'array' },
+    { id: 1, menuOption: 'Testing Team', dataType: 'array' },
+    { id: 2, menuOption: 'Midwest Cover Crops Council', dataType: 'JSX' },
+    { id: 3, menuOption: 'Northeast Cover Crops Council', dataType: 'array' },
+    { id: 4, menuOption: 'Southern Cover Crops Council', dataType: 'array' },
+    { id: 5, menuOption: 'Western Cover Crops Council', dataType: 'JSX' },
   ];
 
   const handleChange = (newValue) => {
@@ -31,26 +31,26 @@ const AboutTheExperts = () => {
 
   return (
     <Box sx={{ border: 0.5, borderColor: 'grey.300' }} ml={2} mr={2} mt={5}>
-      {councils.map((council) => (
+      {expertGroups.map((group) => (
         <Button
-          key={council.id}
+          key={group.id}
           size="Small"
           sx={{
             width: '16.6%',
             minHeight: '61px',
-            backgroundColor: (council.id === value) ? '#598444' : 'white',
-            color: (council.id === value) ? 'white' : '#8abc62',
-            '&:hover': { backgroundColor: (council.id === value) ? '#598444' : 'white' },
+            backgroundColor: (group.id === value) ? '#598444' : 'white',
+            color: (group.id === value) ? 'white' : '#8abc62',
+            '&:hover': { backgroundColor: (group.id === value) ? '#598444' : 'white' },
           }}
-          onClick={() => handleChange(council.id)}
+          onClick={() => handleChange(group.id)}
           variant="contained"
         >
-          {council.menuOption}
+          {group.menuOption}
         </Button>
 
       ))}
       <Typography style={{ paddingTop: '15px' }} variant="body1" align="left">
-        {Array.isArray(getExpertsData(value))
+        {expertGroups[value].dataType === 'array'
           ? getExpertsData(value)
             .sort((a, b) => (a.lastName.localeCompare(b.lastName)))
             .map((expert) => (
