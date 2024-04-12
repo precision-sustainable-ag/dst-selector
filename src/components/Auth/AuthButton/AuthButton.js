d0f3620bb3bbfebf32000897b38ed3806c49aa8b
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2024-04-12 10:40:56 -0400

    fix color contrast issues

--- src/components/Auth/AuthButton/AuthButton.js
+++ src/components/Auth/AuthButton/AuthButton.js
@@ -8,7 +8,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
 
 const AuthButton = ({
-  type, variant = 'text', onClickCallback = () => {}, color = 'primary',
+  type, variant = 'text', onClickCallback = () => {}, color = 'secondary',
 }) => {
   const { loginWithRedirect, logout } = useAuth0();
 
