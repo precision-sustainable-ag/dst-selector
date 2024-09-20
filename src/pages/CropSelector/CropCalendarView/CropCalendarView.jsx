f87db859618ffcfde2c4423fd4a4a405984e3c37
YazidZidane <xjt980408@gmail.com>
2024-09-20 10:09:30 -0400

    fix tests for crop-list

--- src/pages/CropSelector/CropCalendarView/CropCalendarView.jsx
+++ src/pages/CropSelector/CropCalendarView/CropCalendarView.jsx
@@ -167,6 +167,7 @@ const CropCalendarView = ({
                   style={{ marginBottom: '7px' }}
                   startIcon={<ListIcon style={{ fontSize: 'larger' }} />}
                   buttonType="PillButton"
+                  data-cy="crop-list-btn"
                 >
                   CROP LIST
                 </PSAButton>
