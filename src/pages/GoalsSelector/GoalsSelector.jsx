5ae479c3b8a24df53fbcec91a3dd3bb0c2f59fe5
btfarre2 <btfarre2@ncsu.edu>
2025-03-13 21:33:31 -0400

    Implementing Additional Filters

--- src/pages/GoalsSelector/GoalsSelector.jsx
+++ src/pages/GoalsSelector/GoalsSelector.jsx
@@ -5,6 +5,7 @@
 // TODO: Goal tags are not responsive!
 import {
   Typography, Grid, Box, useMediaQuery, useTheme,
+  Chip,
 } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
@@ -30,6 +31,16 @@ const GoalsSelector = () => {
 
   // useState vars
   const [allGoals, setAllGoals] = useState([]);
+
+  const seasons = ['Spring', 'Summer', 'Autumn', 'Winter'];
+  const [selectedSeason, setSelectedSeason] = useState(null);
+
+  const floweringTypes = ['Annual', 'Perennial'];
+  const [selectedFlowering, setSelectedFlowering] = useState(null);
+
+  const irrigationType = ['Rainfed', 'Irrigated'];
+  const [selectedIrrigation, setSelectedIrrigation] = useState(null);
+
   useEffect(() => {
     callCoverCropApi(
       `https://${apiBaseUrlRedux}.covercrop-selector.org/v1/states/${stateIdRedux}/goals?${queryStringRedux}`,
@@ -45,121 +56,312 @@ const GoalsSelector = () => {
   return (
     <Box>
       <Grid container spacing={isLargeScreen ? 4 : 1}>
-        {/* holds goal selector */}
-        <Grid item container lg={6} justifyContent={isLargeScreen ? 'flex-end' : 'center'}>
-          <Grid
-            item
-            container
-            lg={10}
-            sx={{
-              boxSizing: 'border-box',
-              borderRadius: '15px',
-              border: '2px solid #598445',
-              p: '1rem',
-              margin: !isLargeScreen ? '1rem' : '0',
-            }}
-            data-test="goals-card"
-          >
-            {/* title */}
-            <Grid item xs={12}>
-              <Typography variant="h4" align="center" data-test="title-goals">
-                Goals
-              </Typography>
-            </Grid>
-            {/* sub-title */}
-            <Grid item xs={12}>
-              <Typography
-                variant={isMobile ? 'subtitle2' : 'subtitle1'}
-                align="center"
-                gutterBottom
-              >
-                Select 1 to 3 goals in order of importance.
-              </Typography>
-            </Grid>
-            <Grid item xs={12} mb={2}>
-              <Typography variant="subtitle2" gutterBottom align="center">
-                Tap and hold for more information
-              </Typography>
-            </Grid>
-            {/* chips */}
+        {/* top row */}
+        <Grid container item lg={12} spacing={isLargeScreen ? 4 : 1}>
+          {/* holds goal selector */}
+          <Grid item container lg={6} justifyContent={isLargeScreen ? 'flex-end' : 'center'}>
             <Grid
               item
               container
-              spacing={1}
-              justifyContent="center"
-              alignItems="center"
+              lg={10}
               sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                minHeight: '100px',
+                boxSizing: 'border-box',
+                borderRadius: '15px',
+                border: '2px solid #598445',
+                p: '1rem',
+                margin: !isLargeScreen ? '1rem' : '0',
               }}
+              data-test="goals-card"
             >
-              {allGoals?.length > 0 ? (
-                <Grid
-                  item
-                  container
-                  spacing={1}
-                  justifyContent="center"
-                  alignItems="center"
+              {/* title */}
+              <Grid item xs={12}>
+                <Typography variant="h4" align="center" data-test="title-goals">
+                  Goals
+                </Typography>
+              </Grid>
+              {/* sub-title */}
+              <Grid item xs={12}>
+                <Typography
+                  variant={isMobile ? 'subtitle2' : 'subtitle1'}
+                  align="center"
+                  gutterBottom
                 >
-                  {allGoals
-                    .slice()
-                    // Transforming the indexOf -1 from a non selected item to 3 allows the index 0-2 to be avaliable for the selected goals
-                    .sort(
-                      (a, b) => (selectedGoalsRedux.indexOf(a.label) === -1
-                        ? 3
-                        : selectedGoalsRedux.indexOf(a.label))
-                        - (selectedGoalsRedux.indexOf(b.label) === -1
+                  Select 1 to 3 goals in order of importance.
+                </Typography>
+              </Grid>
+              <Grid item xs={12} mb={2}>
+                <Typography variant="subtitle2" gutterBottom align="center">
+                  Tap and hold for more information
+                </Typography>
+              </Grid>
+              {/* chips */}
+              <Grid
+                item
+                container
+                spacing={1}
+                justifyContent="center"
+                alignItems="center"
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  minHeight: '100px',
+                }}
+              >
+                {allGoals?.length > 0 ? (
+                  <Grid
+                    item
+                    container
+                    spacing={1}
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    {allGoals
+                      .slice()
+                      // Transforming the indexOf -1 from a non selected item to 3 allows the index 0-2 to be avaliable for the selected goals
+                      .sort(
+                        (a, b) => (selectedGoalsRedux.indexOf(a.label) === -1
                           ? 3
-                          : selectedGoalsRedux.indexOf(b.label)),
-                    )
-                    .map((goal, key) => (
-                      <Grid
-                        item
-                        key={key}
-                        display="flex"
-                        xs={isMobile ? 12 : 'auto'}
-                        justifyContent="center"
-                        alignItems="center"
-                      >
-                        <GoalTag
+                          : selectedGoalsRedux.indexOf(a.label))
+                          - (selectedGoalsRedux.indexOf(b.label) === -1
+                            ? 3
+                            : selectedGoalsRedux.indexOf(b.label)),
+                      )
+                      .map((goal, key) => (
+                        <Grid
+                          item
                           key={key}
-                          goal={goal}
-                          id={key}
-                          goaltTitle={goal.label}
-                          goalDescription={goal.description}
-                        />
-                      </Grid>
-                    ))}
-                </Grid>
-              ) : (
-                <PSALoadingSpinner />
-              )}
+                          display="flex"
+                          xs={isMobile ? 12 : 'auto'}
+                          justifyContent="center"
+                          alignItems="center"
+                        >
+                          <GoalTag
+                            key={key}
+                            goal={goal}
+                            id={key}
+                            goaltTitle={goal.label}
+                            goalDescription={goal.description}
+                          />
+                        </Grid>
+                      ))}
+                  </Grid>
+                ) : (
+                  <PSALoadingSpinner />
+                )}
+              </Grid>
+            </Grid>
+          </Grid>
+          {/* holds the date selector */}
+          <Grid item container lg={6}>
+            <Grid
+              item
+              container
+              lg={10}
+              sx={{
+                boxSizing: 'border-box',
+                borderRadius: '15px',
+                border: '2px solid #598445',
+                p: '1rem',
+                mr: !isLargeScreen ? '1rem' : '0',
+                ml: !isLargeScreen ? '1rem' : '0',
+                mb: !isLargeScreen ? '1rem' : '0,',
+              }}
+              justifyContent="center"
+              data-test="cashcrop-window"
+            >
+              <PreviousCashCrop />
             </Grid>
           </Grid>
         </Grid>
-        {/* holds the date selector */}
-        <Grid item container lg={6}>
+      </Grid>
+
+      {/* =============================================== */}
+
+      {/* Bottom Row */}
+      <Grid
+        container
+        item
+        lg={8}
+        spacing={isLargeScreen ? 4 : 0}
+        sx={{
+          boxSizing: 'border-box',
+          borderRadius: '15px',
+          border: '2px solid #598445',
+          // margin: !isLargeScreen ? '1rem' : '0',
+          mx: 'auto',
+          mt: isLargeScreen ? 4 : 2,
+          mb: isLargeScreen ? 4 : 2,
+        }}
+        justifyContent="center"
+      >
+        <Grid item xs={12}>
+          <Typography variant="h4" align="center">
+            Additional Filters
+          </Typography>
+          <Typography
+            variant={isMobile ? 'subtitle2' : 'subtitle1'}
+            align="center"
+            gutterBottom
+          >
+            These options will help provide the best cover crop termination information on your information sheet.
+          </Typography>
+
+        </Grid>
+
+        {/* weather grid */}
+        {/* <Grid item container lg={3}> */}
+        <Grid
+          item
+          container
+          lg={4}
+          sx={{
+            p: '1rem',
+            mr: !isLargeScreen ? '1rem' : '0',
+            ml: !isLargeScreen ? '1rem' : '0',
+            mb: !isLargeScreen ? '1rem' : '0,',
+
+          }}
+          data-test="goals-card"
+        >
+          <Grid item xs={12}>
+            <Typography variant="h5" align="center" data-test="title-goals">
+              Season
+            </Typography>
+          </Grid>
+          {/* sub-title */}
           <Grid
             item
-            container
-            lg={10}
+            xs={12}
             sx={{
-              boxSizing: 'border-box',
-              borderRadius: '15px',
-              border: '2px solid #598445',
-              p: '1rem',
-              mr: !isLargeScreen ? '1rem' : '0',
-              ml: !isLargeScreen ? '1rem' : '0',
-              mb: !isLargeScreen ? '1rem' : '0,',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+
             }}
-            justifyContent="center"
-            data-test="cashcrop-window"
           >
-            <PreviousCashCrop />
+            {seasons.map((season) => (
+              <Chip
+                key={season}
+                label={season}
+                clickable
+                style={{ margin: '0.3rem' }}
+                sx={{
+                  '&:focus': {
+                    boxShadow: '0 0 0 2px black',
+                    maxWidth: !isLargeScreen ? '45%' : 'auto',
+                  },
+                }}
+                onClick={() => setSelectedSeason(season)}
+                color={selectedSeason === season ? 'primary' : 'secondary'}
+              />
+            ))}
+
           </Grid>
         </Grid>
+
+        {/* Annual/Perennial */}
+        <Grid
+          item
+          container
+          lg={4}
+          sx={{
+            // border: '2px solid #598445',
+            p: '1rem',
+            mr: !isLargeScreen ? '1rem' : '0',
+            ml: !isLargeScreen ? '1rem' : '0',
+            mb: !isLargeScreen ? '1rem' : '0,',
+          }}
+          justifyContent="center"
+          data-test="cashcrop-window"
+        >
+          <Grid item xs={12}>
+            <Typography variant="h5" align="center" data-test="title-goals">
+              Flowering Type
+            </Typography>
+          </Grid>
+          {/* sub-title */}
+          <Grid
+            item
+            xs={12}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            {floweringTypes.map((floweringType) => (
+              <Chip
+                key={floweringType}
+                label={floweringType}
+                clickable
+                style={{ margin: '0.3rem' }}
+                sx={{
+                  '&:focus': {
+                    boxShadow: '0 0 0 2px black',
+                  },
+                }}
+                onClick={() => setSelectedFlowering(floweringType)}
+                color={selectedFlowering === floweringType ? 'primary' : 'secondary'}
+              />
+            ))}
+
+          </Grid>
+
+        </Grid>
+
+        {/* Water */}
+        <Grid
+          item
+          container
+          lg={4}
+          sx={{
+            boxSizing: 'border-box',
+            borderRadius: '15px',
+            // border: '2px solid #598445',
+            p: '1rem',
+            mr: !isLargeScreen ? '1rem' : '0',
+            ml: !isLargeScreen ? '1rem' : '0',
+            mb: !isLargeScreen ? '1rem' : '0,',
+          }}
+          justifyContent="center"
+          data-test="cashcrop-window"
+        >
+          <Grid item xs={12}>
+            <Typography variant="h5" align="center" data-test="title-goals">
+              Irrigation Type
+            </Typography>
+          </Grid>
+          {/* sub-title */}
+          <Grid
+            item
+            xs={12}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            {irrigationType.map((irrigation) => (
+              <Chip
+                key={irrigation}
+                label={irrigation}
+                clickable
+                style={{ margin: '0.3rem' }}
+                sx={{
+                  '&:focus': {
+                    boxShadow: '0 0 0 2px black',
+                  },
+                }}
+                onClick={() => setSelectedIrrigation(irrigation)}
+                color={selectedIrrigation === irrigation ? 'primary' : 'secondary'}
+              />
+            ))}
+
+          </Grid>
+        </Grid>
+
+        {/* </Grid> */}
       </Grid>
     </Box>
   );
