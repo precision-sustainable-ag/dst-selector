8ff036b440071c07b77e900daea88e927649c213
Jingtong Xu <xjt980408@gmail.com>
2025-04-29 10:00:52 -0400

    replace crop card

--- src/components/CropCard/CropCard.jsx
+++ src/components/CropCard/CropCard.jsx
@@ -1,19 +1,20 @@
-import {
-  Card, CardActionArea, CardContent, CardMedia, Typography, Box,
-} from '@mui/material';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
 import React, {
   useEffect, useState,
 } from 'react';
-import { PSAButton } from 'shared-react-components/src';
+import { PSACropCard } from 'shared-react-components/src';
 import { useSelector } from 'react-redux';
-import { addCropToBasket, trimString } from '../../shared/constants';
+import { addCropToBasket } from '../../shared/constants';
 import { myCropListLocation, snackHandler } from '../../reduxStore/sharedSlice';
 import { updateSelectedCropIds } from '../../reduxStore/cropSlice';
 import { setSaveHistory } from '../../reduxStore/userSlice';
+// import CropDetailsModal from '../CropDetailsModal/CropDetailsModal';
+import InformationSheetContent from '../../pages/InformationSheetContent/InformationSheetContent';
+import { InfoSheetTitle } from '../CropDetailsModal/CropDetailsModal';
 
 const CropCard = ({
+  // eslint-disable-next-line no-unused-vars
   crop, handleModalOpen, index, dispatchRedux,
 }) => {
   // used to know if the user is in mobile mode
@@ -27,8 +28,6 @@ const CropCard = ({
 
   // useState vars
   const [selectedBtns, setSelectedBtns] = useState(selectedCropIdsRedux);
-
-  // TODO: Update SelectedCropsRedux
 
   async function updateBtns() {
     await setSelectedBtns(selectedCropIdsRedux);
@@ -62,87 +61,42 @@ const CropCard = ({
   const hasExcessiveDrainage = crop.soilDrainage?.includes('Excessively drained');
   const shouldHighlightRed = hasExcessiveDrainage && soilDrainageFilterRedux;
 
-  // height: isMobile ? '350px' : '350px'
   return (
-    <Card style={{ width: isMobile ? '160px' : '260px' }} data-test={`crop-card-${index - 1}`}>
-      <CardActionArea onClick={() => handleModalOpen(crop)}>
-        <Box
-          sx={{
-            height: 140,
-            overflow: 'hidden',
-          }}
-        >
-          <CardMedia
-            component="img"
-            image={
-              isMobile
-                ? crop.thumbnailMobile || 'https://placehold.co/160x140?text=Placeholder'
-                : crop.thumbnailWide || 'https://placehold.co/260x140?text=Placeholder'
-            }
-            sx={{
-              height: needsRotation(crop) ? 'auto' : '100%',
-              width: needsRotation(crop) ? '100%' : 'auto',
-              transform: needsRotation(crop) ? 'rotate(90deg) scale(1.9)' : 'none',
-              transformOrigin: 'center',
-              borderWidth: shouldHighlightRed ? '4px' : '0px',
-              borderColor: shouldHighlightRed ? 'red' : 'transparent',
-              borderStyle: 'solid',
-            }}
-            title={crop.label}
-          />
-        </Box>
-      </CardActionArea>
-      <CardContent>
-        <Typography
-          sx={{ color: 'grey', textTransform: 'uppercase' }}
-        >
-          {crop.attributes.filter((a) => a.label === 'Cover Crop Group')[0]?.values[0].value}
-        </Typography>
-        <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }} data-test={`crop-card-label-${index - 1}`}>
-          {crop.label}
-        </Typography>
-        <Typography sx={{ fontStyle: 'italic' }}>
-          {crop.scientificName ? trimString(crop.scientificName, 25) : 'No Data'}
-        </Typography>
-        <PSAButton
-          style={{
-            fontSize: '10pt',
-            left: -5,
-            textDecoration: 'underline',
-            color: 'rgb(53, 153, 155)',
-            backgroundColor: 'transparent',
-          }}
-          buttonType=""
-          target="_blank"
-          onClick={() => handleModalOpen(crop)}
-          title="View Crop Details"
-        />
-      </CardContent>
-      <CardActionArea
-        id={`cartBtn${index}`}
-        sx={{
-          backgroundColor: selectedBtns.includes(crop.id) ? '#2b7b79' : '#e3f2f4',
-          color: selectedBtns.includes(crop.id) ? 'white' : 'black',
-          textAlign: 'center',
-          padding: '0.5em',
-        }}
-        onClick={() => (
-          addToBasket(
-            crop.id,
-            crop.label,
-          )
-        )}
-      >
-        <Typography
-          variant="body2"
-          sx={{ color: selectedBtns.includes(crop.id) ? 'white' : '', fontWeight: 'bold' }}
-        >
-          {selectedBtns.includes(crop.id)
-            ? 'REMOVE'
-            : 'ADD TO LIST'}
-        </Typography>
-      </CardActionArea>
-    </Card>
+    <PSACropCard
+      species={crop.label}
+      scientific={crop.scientificName}
+      details={<InformationSheetContent crop={crop} />}
+      title={<InfoSheetTitle />}
+      thumbnail={crop.thumbnailWide}
+      fullsize={crop.thumbnail}
+      portrait={crop.thumbnail}
+      selected={selectedBtns.includes(crop.id)}
+      onSelect={() => {
+        addToBasket(
+          crop.id,
+          crop.label,
+        );
+      }}
+      onRemove={() => {
+        addToBasket(
+          crop.id,
+          crop.label,
+        );
+      }}
+      sx={{
+        minHeight: '',
+        '.heading': {
+          minHeight: '',
+        },
+        img: {
+          left: 0,
+          transform: needsRotation(crop) ? 'rotate(90deg) scale(1.9)' : 'none',
+          border: shouldHighlightRed ? '4px solid red' : 'none',
+        },
+        marginBottom: '1rem',
+      }}
+      infoSheetProps={{ fullScreen: isMobile }}
+    />
   );
 };
 
