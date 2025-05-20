9be88a2c8336a0a455b04beaf7890c806174a841
Jingtong Xu <xjt980408@gmail.com>
2025-05-20 15:53:29 -0400

    update api url for pipeline env, fix location
    set marker on landing page when select any states

--- src/reduxStore/sharedSlice.jsx
+++ src/reduxStore/sharedSlice.jsx
@@ -1,3 +1,5 @@
+import { testAuth0Env } from '../shared/keys';
+
 const initialState = {
   progress: 0,
   printing: false,
@@ -9,7 +11,7 @@ const initialState = {
   comparisonKeys: [],
   myCoverCropListLocation: '',
   regionToggle: true,
-  apiBaseUrl: /(localhost|dev)/i.test(window.location)
+  apiBaseUrl: testAuth0Env || /(localhost|dev)/i.test(window.location)
     ? 'developapi'
     : 'api',
   queryString: null,
