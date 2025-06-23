e4435ef8c867f6424257fe659189693f10601636
Jingtong Xu <xjt980408@gmail.com>
2025-06-23 13:36:03 -0400

    remove irrigation filtering

--- src/pages/CropSidebar/CropSidebar.jsx
+++ src/pages/CropSidebar/CropSidebar.jsx
@@ -188,10 +188,7 @@ const CropSidebar = ({
       if (councilShorthandRedux === 'WCCC') {
         match = false;
         const seasonMatch = selectedSeasonRedux.length === 0
-        || crop.plantingDates.some(
-          (date) => selectedSeasonRedux.some((season) => date.label.includes(season))
-            && date.label.includes(irrigationFilterRedux ? 'irrigation' : 'rainfed'),
-        );
+        || crop.plantingDates.some((date) => selectedSeasonRedux.some((season) => date.label.includes(season)));
         const firstGoalRatingLargerThanTwo = selectedGoalsRedux.length === 0
         || Number(crop.goals.filter((g) => g.label === selectedGoalsRedux[0])[0]?.values[0]?.value) > 2;
         if (seasonMatch && firstGoalRatingLargerThanTwo) {
