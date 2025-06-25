2c2d17a1eaf703aeecd120d5a218ceaaf8a632df
Ravi Ghevariya <ravighevariya10@gmail.com>
2025-06-25 11:14:12 -0400

    Removed the unnecessary useEffect

--- src/pages/MyCoverCropList/MyCoverCropListWrapper/MyCoverCropListWrapper.jsx
+++ src/pages/MyCoverCropList/MyCoverCropListWrapper/MyCoverCropListWrapper.jsx
@@ -9,14 +9,9 @@ import { useDispatch } from 'react-redux';
 import CropSidebar from '../../CropSidebar/CropSidebar';
 import MyCoverCropList from '../MyCoverCropList';
 import { setSidebarWidth } from '../../../reduxStore/pageSlice';
-import pirschAnalytics from '../../../shared/analytics';
 
 const MyCoverCropListWrapper = () => {
   const [comparisonView, setComparisonView] = useState(true);
-
-  useEffect(() => {
-    pirschAnalytics(comparisonView ? 'Selected Crops: Comparison View' : 'Selected Crops: Crop List');
-  }, [comparisonView]);
 
   const sidebarRef = useRef(null);
   const dispatchRedux = useDispatch();
