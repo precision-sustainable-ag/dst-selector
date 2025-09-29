95455afad33aeacad254b0229eab2ffc67259fad
Ravi Ghevariya <ravighevariya10@gmail.com>
2025-09-29 10:25:49 -0400

    wizard infinity callback stopper

--- src/pages/Wizard/wizard.jsx
+++ src/pages/Wizard/wizard.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import { PSAWizard } from 'shared-react-components/src';
 
-const Wizard = () => <PSAWizard />;
+const Wizard = () => {
+  const savedResult = localStorage.getItem('recommendedApp');
+
+  const result = () => {
+    window.location.href = JSON.parse(savedResult);
+  };
+
+  const handleWizardFinish = (mainResult) => {
+    if (mainResult) {
+      localStorage.setItem('recommendedApp', JSON.stringify(mainResult));
+    }
+  };
+
+  return (
+    <div>
+      {savedResult && (result())}
+      {!savedResult && <PSAWizard onFinish={handleWizardFinish} />}
+    </div>
+  );
+};
 
 export default Wizard;
