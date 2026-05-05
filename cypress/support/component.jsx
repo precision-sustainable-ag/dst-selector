d83a6042efb1294910b52523353eed6866fb3ff8
Ravi Ghevariya <ravighevariya10@gmail.com>
2026-05-05 15:30:50 -0400

    remove Auth0 context provider from Cypress mount command

--- cypress/support/component.jsx
+++ cypress/support/component.jsx
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { dstTheme } from '../../src/App';
 import configureStore from '../../src/reduxStore/store';
-import { Auth0Context } from '@auth0/auth0-react';
 
 /* eslint-disable no-undef */
 
@@ -16,23 +15,12 @@ Cypress.Commands.add('mount', (component, options = {}) => {
     win.document.getElementById = cy.stub().withArgs('favicon').returns({ href: '' });
   });
 
-  const auth0Value = {
-    isAuthenticated: false,
-    isLoading: false,
-    user: null,
-    getAccessTokenSilently: cy.stub().resolves('mock-token'),
-    loginWithRedirect: cy.stub(),
-    logout: cy.stub(),
-  };
-
   const wrapped = (
-    <Auth0Context.Provider value={auth0Value}>
-      <Provider store={reduxStore}>
-        <ThemeProvider theme={dstTheme}>
-          <BrowserRouter>{component}</BrowserRouter>
-        </ThemeProvider>
-      </Provider>
-    </Auth0Context.Provider>
+    <Provider store={reduxStore}>
+      <ThemeProvider theme={dstTheme}>
+        <BrowserRouter>{component}</BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   );
 
   return mount(wrapped, mountOptions);
