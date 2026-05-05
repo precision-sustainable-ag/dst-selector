9c05ce3de5214f8c9bd0db95e9f183b83c9c4e27
Ravi Ghevariya <70748055+ravighevariya10@users.noreply.github.com>
2026-05-05 12:06:42 -0400

    Fix logo image source assertion in landing-e2e test

--- cypress/e2e/landing-e2e.cy.js
+++ cypress/e2e/landing-e2e.cy.js
@@ -43,7 +43,7 @@ describe('Test all possible interactions on the Landing Page after a state is se
         cy.getByTestId('header_logo_button')
           .find('img')
           .invoke('attr', 'src')
-          .should('include', `images/${councilShorthand.toLowerCase()}_logo`);
+          .should('include', `${councilShorthand.toLowerCase()}_logo`);
       });
   });
 
